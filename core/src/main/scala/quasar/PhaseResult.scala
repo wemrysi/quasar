c83f08c3f0e330a7b44bf710b58069947065d819
Emrys Ingersoll <ingersoll@gmail.com>
2015-10-28 17:25:12 -0500

    ExecutePlan Functor describing LP execution

---
+++ core/src/main/scala/quasar/PhaseResult.scala
@@ -0,0 +1,32 @@
+package quasar
+
+import quasar.Predef._
+
+import argonaut._, Argonaut._
+import scalaz.Show
+
+sealed trait PhaseResult {
+  def name: String
+}
+
+object PhaseResult {
+  final case class Tree(name: String, value: RenderedTree) extends PhaseResult {
+    override def toString = name + "\n" + Show[RenderedTree].shows(value)
+  }
+  final case class Detail(name: String, value: String) extends PhaseResult {
+    override def toString = name + "\n" + value
+  }
+
+  implicit def phaseResultEncodeJson: EncodeJson[PhaseResult] = EncodeJson {
+    case PhaseResult.Tree(name, value) =>
+      Json.obj(
+        "name" := name,
+        "tree" := value
+      )
+    case PhaseResult.Detail(name, value) =>
+      Json.obj(
+        "name"   := name,
+        "detail" := value
+      )
+  }
+}
