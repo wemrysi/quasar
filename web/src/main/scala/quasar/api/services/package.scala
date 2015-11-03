c8514f825a2c667f8fc4f9c18ede31ffcd390cd2
Jean-Rémi Desjardins <jeanremi.desjardins@gmail.com>
2015-11-03 14:51:28 -0700

    Clean up error messages

--- web/src/main/scala/quasar/api/services/package.scala
+++ web/src/main/scala/quasar/api/services/package.scala
@@ -2,16 +2,11 @@ package quasar.api
 
 import quasar.Predef._
 
-import argonaut._
-import argonaut.Argonaut._
-import org.http4s.argonaut._
-
 import org.http4s.{EntityEncoder, Response}
 import org.http4s.dsl._
-import org.http4s.headers.{`Content-Disposition`, `Content-Type`}
+import org.http4s.headers.`Content-Type`
 
 import quasar.fs.FileSystemError._
-import quasar.fs.PathError2.{InvalidPath0, PathExists0, PathNotFound0}
 import quasar.fs._
 import quasar.fp._
 
@@ -26,22 +21,19 @@ package object services {
   def fileSystemErrorResponse(error: FileSystemError): Task[Response] =
     error.fold(
       pathErrorResponse,
-      unknownReadHandle => BadRequest("unknown read handle"),
-      unknownWriteHandle => BadRequest("unknown write handle"),
-      partialWrite => BadRequest("Partial write"),
-      (data, str) => BadRequest("some other thing")
+      unknownReadHandle => InternalServerError(s"Unknown read handle: $unknownReadHandle"),
+      unknownWriteHandle => InternalServerError(s"Unknown write handle: $unknownWriteHandle"),
+      numFailed => InternalServerError(s"Failed to write $numFailed records"),
+      (data, reason) => InternalServerError(s"Failed to write ${data.shows} because of $reason")
     )
 
   def pathErrorResponse(error: PathError2): Task[Response] =
     error.fold(
-      path => Conflict("path already exists"),
+      path => Conflict(s"$path already exists"),
       // TODO: Adjust definition of `AbsPath` in order to avoid this fold...
       path => NotFound(s"${path.fold(posixCodec.printPath,posixCodec.printPath)}: doesn't exist"),
-      (path, reason) => BadRequest("invalid path")
+      (path, reason) => BadRequest(s"$path is an invalid path because $reason")
     )
-
-//  def failureResponse(f: Json => Task[Response], message: String): Task[Response] =
-//    f(Json("error" := message))
 
   type FilesystemTask[A] = FileSystemErrT[Task, A]
   /** Flatten by inserting the [[quasar.fs.FileSystemError]] into the failure case of the [[scalaz.concurrent.Task]] */
