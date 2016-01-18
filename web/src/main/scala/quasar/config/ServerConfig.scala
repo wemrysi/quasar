d187f8e21152f15aefd7b384267d7b959f360681
Emrys Ingersoll <ingersoll@gmail.com>
2016-01-18 15:48:33 -0600

    Move ServerConfig to its own file

--- web/src/main/scala/quasar/config/ServerConfig.scala
+++ web/src/main/scala/quasar/config/ServerConfig.scala
@@ -16,23 +16,20 @@
 
 package quasar.config
 
-import quasar.Predef._
-import quasar.config.implicits._
+import quasar.Predef.{Int, Option, Some}
 
 import argonaut._, Argonaut._
-import monocle._, macros.Lenses
+import monocle.Lens
 
-@Lenses final case class WebConfig(server: ServerConfig, mountings: MountingsConfig)
-
-object WebConfig extends ConfigOps[WebConfig] {
-  def mountingsLens = WebConfig.mountings
-
-  implicit val codecJson: CodecJson[WebConfig] =
-    casecodec2(WebConfig.apply, WebConfig.unapply)("server", "mountings")
+final case class ServerConfig(port0: Option[Int]) {
+  val port = port0.getOrElse(ServerConfig.DefaultPort)
 }
 
-final case class WebConfigLens[WC, SC](
-  server: Lens[WC, SC],
-  mountings: Lens[WC, MountingsConfig],
-  wcPort: Lens[WC, Int],
-  scPort: Lens[SC, Int])
+object ServerConfig {
+   val DefaultPort = 20223
+
+   val port = Lens[ServerConfig, Int](_.port)(p => c => c.copy(port0 = Some(p)))
+
+   implicit def Codec: CodecJson[ServerConfig] =
+     casecodec1(ServerConfig.apply, ServerConfig.unapply)("port")
+}
