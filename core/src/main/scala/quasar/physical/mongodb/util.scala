92f208fa09d7e8024c62946568475fcd7b435e42
Emrys Ingersoll <ingersoll@gmail.com>
2016-01-27 18:11:18 -0600

    Update to mongo-java driver 3.2.1, test connection when creating async client

--- core/src/main/scala/quasar/physical/mongodb/util.scala
+++ core/src/main/scala/quasar/physical/mongodb/util.scala
@@ -24,13 +24,13 @@ import quasar.fp.free
 import quasar.fp.prism._
 import quasar.fs.mount.{ConnectionUri, FileSystemDef}
 
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.{TimeUnit, TimeoutException}
 
 import com.mongodb._
 import com.mongodb.async.client.{MongoClient => AMongoClient, MongoClients, MongoClientSettings}
-import com.mongodb.connection.ClusterSettings
+import com.mongodb.connection.{ClusterSettings, SocketSettings, SslSettings}
 import scalaz._
-import scalaz.concurrent.Task
+import scalaz.concurrent.{Strategy, Task}
 import scalaz.syntax.applicative._
 
 object util {
@@ -39,6 +39,13 @@ object util {
   def createMongoClient(config: MongoDbConfig): Task[MongoClient] =
     disableMongoLogging *> mongoClient(config.uri)
 
+  /** Returns an async `MongoClient` for the given `ConnectionUri`. Will fail
+    * with a `ConfigError` if the uri is invalid and with an `EnvironmentError`
+    * if there is a problem connecting to the server.
+    *
+    * NB: The connection is tested during creation and creation will fail if
+    *     connecting to the server times out.
+    */
   def createAsyncMongoClient[S[_]: Functor](
     uri: ConnectionUri
   )(implicit
@@ -63,25 +70,65 @@ object util {
           DefaultClusterSettingsBuilder
             .applyConnectionString(cs)
             .build())
+        .socketSettings(
+          SocketSettings.builder()
+            .applyConnectionString(cs)
+            build())
+        .sslSettings(
+          SslSettings.builder()
+            .applyConnectionString(cs)
+            .build())
         .build()
 
+    /** Attempts a benign operation (reading the server version) using the
+      * given client in order to test whether the connection was successful,
+      * necessary as otherwise, given a bad connection URI, the driver will
+      * retry indefinitely, on a separate monitor thread, to test the connection
+      * while the next operation on the returned `MongoClient` will just block
+      * indefinitely (i.e. somewhere in userland).
+      *
+      * TODO: Is there a better way to achieve this? Or somewhere we can set
+      *       a timeout for the driver to consider an operation that takes
+      *       too long an error?
+      */
+    def testConnection(aclient: AMongoClient): Task[Unit] =
+      MongoDbIO.serverVersion.run(aclient)
+        .timed(defaultTimeoutMillis)(Strategy.DefaultTimeoutScheduler)
+        .attempt flatMap {
+          case -\/(tout: TimeoutException) =>
+            val hosts = aclient.getSettings.getClusterSettings.getHosts.toString
+            Task.fail(new TimeoutException(s"Timed out attempting to connect to: $hosts"))
+          case -\/(t) =>
+            Task.fail(t)
+          case \/-(_) =>
+            Task.now(())
+        }
+
     def createClient(settings: MongoClientSettings) =
-      liftAndHandle(Task.delay(MongoClients.create(settings)))(t =>
-        envErr.fail(connectionFailed(t.getMessage)))
+      liftAndHandle(for {
+        client <- Task.delay(MongoClients.create(settings))
+        _      <- testConnection(client) onFinish {
+                    case Some(_) => Task.delay(client.close())
+                    case None    => Task.now(())
+                  }
+      } yield client)(t => envErr.fail(connectionFailed(t.getMessage)))
 
     disableLogging *> connString map clientSettings >>= createClient
   }
 
   ////
 
+  // TODO: Externalize
+  private val defaultTimeoutMillis: Int = 5000
+
   private val DefaultOptions =
     (new MongoClientOptions.Builder)
-      .serverSelectionTimeout(5000)
+      .serverSelectionTimeout(defaultTimeoutMillis)
       .build
 
   private def DefaultClusterSettingsBuilder =
     ClusterSettings.builder()
-      .serverSelectionTimeout(5000, TimeUnit.MILLISECONDS)
+      .serverSelectionTimeout(defaultTimeoutMillis, TimeUnit.MILLISECONDS)
 
   private val mongoClient: ConnectionString => Task[MongoClient] = {
     val memo = Memo.mutableHashMapMemo[ConnectionString, MongoClient] { (uri: ConnectionString) =>
