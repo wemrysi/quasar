413ed13e3c522afb32d0ab192b65910c073bf9e1
Greg Pfeil <greg@technomadic.org>
2016-02-02 17:07:14 -0700

    Various improvements to our SBT config.
    This was triggered by newer versions of Ensime not working with our
    repository. The two changes that were necessary to fix that were
    
    * Don’t explicitly include `Defaults.defaultSettings` in our settings –
      it tramples various plugin settings, and is deprecated.
    * Include our settings on the root project, so we don’t also fetch scala
      2.10 libraries.
    
    Other changes:
    
    * added compiler options to be used by the meta-project build (these
      would have caught one of the primary issues above),
    * made the Java version check failure include the version it _did_ find,
    * replaced `CustomKeys` with a more comprehensive `Dependencies` object,
    * removed the `NextVersion` steps in the release process,
    * upgraded a couple dependencies,
    * clean up the .gitignore (unignoring `project/project`, and simpifying
      the redundant `target` ignores), and
    * unify compiler flags between project and meta-project[†].
    
    I also modifed the sbt-extras launcher to work with our use of
    `-Yno-imports`. I submitted it upstream, so hopefully it’ll get merged
    and Travis will update their copy, then we can remove our own copy.
    
    There is still more to be done here. Ideally, we’d almost completely
    eliminate .sbt files and do everything in .scala files, but this is an
    improvement.
    
    [†]: the couple flags that are only enabled on the meta-project will be
    fixed in the main project in a follow-up PR.

---
+++ project/Dependencies.scala
@@ -0,0 +1,49 @@
+package quasar.project
+
+import scala.collection.Seq
+
+import sbt._, Keys._
+
+object Dependencies {
+  private val scalazVersion  = "7.1.4"
+  private val slcVersion     = "0.4"
+  private val monocleVersion = "1.1.1"
+  private val pathyVersion   = "0.0.3"
+  private val http4sVersion  = "0.10.1"
+  private val mongoVersion   = "3.2.1"
+
+  val core = Seq(
+    "org.scalaz"        %% "scalaz-core"               % scalazVersion  % "compile, test",
+    "org.scalaz"        %% "scalaz-concurrent"         % scalazVersion  % "compile, test",
+    "org.scalaz.stream" %% "scalaz-stream"             % "0.7.3a"       % "compile, test",
+    "com.github.julien-truffaut" %% "monocle-core"     % monocleVersion % "compile, test",
+    "com.github.julien-truffaut" %% "monocle-generic"  % monocleVersion % "compile, test",
+    "com.github.julien-truffaut" %% "monocle-macro"    % monocleVersion % "compile, test",
+    "com.github.scopt"  %% "scopt"                     % "3.3.0"        % "compile, test",
+    "org.threeten"      %  "threetenbp"                % "1.2"          % "compile, test",
+    "org.mongodb"       %  "mongo-java-driver"         % mongoVersion   % "compile, test",
+    "org.mongodb"       %  "mongodb-driver-async"      % mongoVersion   % "compile, test",
+    "io.argonaut"       %% "argonaut"                  % "6.1"          % "compile, test",
+    "org.jboss.aesh"    %  "aesh"                      % "0.55"         % "compile, test",
+    "org.typelevel"     %% "shapeless-scalaz"          % slcVersion     % "compile, test",
+    "com.slamdata"      %% "pathy-core"                % pathyVersion   % "compile",
+    "com.github.mpilquist" %% "simulacrum"             % "0.4.0"        % "compile, test",
+    "org.http4s"        %% "http4s-core"               % http4sVersion  % "compile",
+    "com.slamdata"      %% "pathy-scalacheck"          % pathyVersion   % "test",
+    "org.scalaz"        %% "scalaz-scalacheck-binding" % scalazVersion  % "test",
+    "org.specs2"        %% "specs2-core"               % "2.4"          % "test",
+    "org.scalacheck"    %% "scalacheck"                % "1.11.6"       % "test" force(),
+    "org.typelevel"     %% "scalaz-specs2"             % "0.3.0"        % "test",
+    "org.typelevel"     %% "shapeless-scalacheck"      % slcVersion     % "test")
+
+  val web = Seq(
+    "org.http4s"           %% "http4s-dsl"          % http4sVersion % "compile, test",
+    "org.http4s"           %% "http4s-argonaut"     % http4sVersion % "compile, test"
+      // TODO: remove once jawn-streamz is in agreement with http4s on
+      //       scalaz-stream version
+      exclude("org.scalaz.stream", "scalaz-stream_2.11"),
+    "org.http4s"           %% "http4s-blaze-server" % http4sVersion % "compile, test",
+    "org.http4s"           %% "http4s-blaze-client" % http4sVersion % "test",
+    "com.github.tototoshi" %% "scala-csv"           % "1.1.2",
+    "org.scodec"           %% "scodec-scalaz"       % "1.1.0")
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -7,3 +7,27 @@ addSbtPlugin("com.github.gseitz"     % "sbt-release"     % "0.8.5")
 addSbtPlugin("org.brianmckenna"      % "sbt-wartremover" % "0.14")
 addSbtPlugin("de.heikoseeberger"     % "sbt-header"      % "1.5.0")
 addSbtPlugin("com.eed3si9n"          % "sbt-buildinfo"   % "0.5.0")
+
+val commonScalacOptions = Seq(
+  "-deprecation",
+  "-encoding", "UTF-8",
+  "-feature",
+  "-language:existentials",
+  "-language:higherKinds",
+  "-language:implicitConversions",
+  "-unchecked",
+  "-Xfatal-warnings",
+  "-Xfuture",
+  "-Yno-adapted-args",
+  "-Yno-imports",
+  "-Ywarn-dead-code",
+  "-Ywarn-value-discard")
+
+// NB: These options trigger issues that need to be fixed in the main project.
+scalacOptions ++= commonScalacOptions ++ Seq("-Xlint", "-Ywarn-numeric-widen")
+
+buildInfoKeys := Seq[BuildInfoKey]("scalacOptions" -> commonScalacOptions)
+
+buildInfoPackage := "quasar.project.build"
+
+lazy val meta = project.in(file(".")).enablePlugins(BuildInfoPlugin)
