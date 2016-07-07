be50ebe10e2d8dc65c1f4102e06b011f822c005b
Paul Phillips <paulp@improving.org>
2016-07-07 09:38:42 -0600

    Combine util and common projects.

--- build.sbt
+++ build.sbt
@@ -1,22 +1,13 @@
 import precog.PlatformBuild._
 
-lazy val platform = project.root aggregate (util, common, yggdrasil, mimir)
+lazy val platform = project.root aggregate (blueeyes, common, yggdrasil, mimir)
 lazy val logging  = project.setup  // common project for the test log configuration files
-lazy val util     = project.setup.testLogging deps (
 
-  "joda-time"              % "joda-time"        % "1.6.2",
-  "javolution"             % "javolution"       % "5.5.1",
-  "org.xlightweb"          % "xlightweb"        % "2.13.2",
-  "com.google.guava"       % "guava"            %  "12.0",
-  "commons-io"             % "commons-io"       %  "2.5",
-  "com.rubiconproject.oss" % "jchronic"         % "0.2.6",
-  "javax.mail"             % "mail"             % "1.4.7",
-  "org.fusesource.scalate" % "scalate-core_2.9" %  "1.6.1"
-)
-lazy val common   = project.setup.testLogging dependsOn util deps (
-  "com.chuusai" %% "shapeless" % "1.2.3"
-)
-lazy val yggdrasil = project.assemblyProject.usesCommon deps (
+/** This used to be the evaluator project.
+ */
+lazy val mimir = project.setup dependsOn yggdrasil.inBothScopes
+
+lazy val yggdrasil = project.setupAssembly dependsOn common.inBothScopes deps (
   "com.reportgrid"       %% "blueeyes-mongo"     % blueeyesVersion,
   "org.objectweb.howl"    % "howl"               %    "1.0.1-1",
   "org.slamdata"          % "jdbm"               %     "3.0.0",
@@ -25,14 +16,21 @@ lazy val yggdrasil = project.assemblyProject.usesCommon deps (
   "org.spire-math"        % "spire_2.9.2"        %     "0.3.0",
   "com.typesafe.akka"     % "akka-testkit"       %     "2.0.5"      % "test"
 )
-
-lazy val blueeyes = project also commonSettings deps (
+lazy val common = project.setup dependsOn (logging.inTestScope, blueeyes) deps (
+  "com.chuusai"            %% "shapeless"        % "1.2.3",
+  "joda-time"               % "joda-time"        % "1.6.2",
+  "javolution"              % "javolution"       % "5.5.1",
+  "org.xlightweb"           % "xlightweb"        % "2.13.2",
+  "com.google.guava"        % "guava"            %  "12.0",
+  "commons-io"              % "commons-io"       %  "2.5",
+  "com.rubiconproject.oss"  % "jchronic"         % "0.2.6",
+  "javax.mail"              % "mail"             % "1.4.7",
+  "org.fusesource.scalate"  % "scalate-core_2.9" %  "1.6.1"
+)
+lazy val blueeyes = project.setup deps (
   "com.reportgrid" %% "blueeyes-json" % blueeyesVersion,
   "com.reportgrid" %% "blueeyes-core" % blueeyesVersion,
   "com.reportgrid" %% "akka_testing"  % blueeyesVersion excludeAll(
     ExclusionRule(organization = "com.reportgrid")
   )
 )
-/** This used to be the evaluator project.
- */
-lazy val mimir = project.setup.usesCommon.testLogging dependsOn (util.inBothScopes, yggdrasil.inBothScopes)
--- project/Build.scala
+++ project/Build.scala
@@ -21,6 +21,8 @@
 package precog
 
 import sbt._, Keys._
+import sbtassembly.AssemblyPlugin.assemblySettings
+import sbtassembly.AssemblyPlugin.autoImport._
 
 object PlatformBuild {
   val Both            = "compile->compile;test->test"
@@ -61,39 +63,37 @@ object PlatformBuild {
     javaOptions in profileTask <<= (javaOptions, baseDirectory) map ((opts, b) => opts :+ jprofilerArg(b))
   )
 
+  def assemblyProjectSettings = assemblySettings ++ Seq(
+                     test in assembly :=  (),
+          assemblyJarName in assembly :=  "%s-assembly-%s.jar".format(name.value, "git describe".!!.trim),
+                   target in assembly <<= target,
+    assemblyMergeStrategy in assembly :=  assemblyMerger
+  )
+  // https://github.com/sbt/sbt-assembly
+  //
+  // By the way, the first case pattern in the above using PathList(...) is how
+  // you can pick javax/servlet/* from the first jar. If the default
+  // MergeStrategy.deduplicate is not working for you, that likely means you have
+  // multiple versions of some library pulled by your dependency graph. The real
+  // solution is to fix that dependency graph. You can work around it by
+  // MergeStrategy.first but don't be surprised when you see
+  // ClassNotFoundException.
+  private def assemblyMerger(path: String) = path match {
+    case s if s endsWith ".txt"                    => MergeStrategy.discard
+    case PathList("META-INF", "MANIFEST.MF")       => MergeStrategy.discard
+    case PathList("org", "slf4j", "impl", xs @ _*) => MergeStrategy.first
+    case _                                         => MergeStrategy.deduplicate
+  }
+
   implicit class ProjectOps(val p: sbt.Project) {
-    import sbtassembly.AssemblyPlugin.assemblySettings
-    import sbtassembly.AssemblyPlugin.autoImport._
-
-    // https://github.com/sbt/sbt-assembly
-    //
-    // By the way, the first case pattern in the above using PathList(...) is how
-    // you can pick javax/servlet/* from the first jar. If the default
-    // MergeStrategy.deduplicate is not working for you, that likely means you have
-    // multiple versions of some library pulled by your dependency graph. The real
-    // solution is to fix that dependency graph. You can work around it by
-    // MergeStrategy.first but don't be surprised when you see
-    // ClassNotFoundException.
-    private def assemblyMerger(path: String) = path match {
-      case s if s endsWith ".txt"                    => MergeStrategy.discard
-      case PathList("META-INF", "MANIFEST.MF")       => MergeStrategy.discard
-      case PathList("org", "slf4j", "impl", xs @ _*) => MergeStrategy.first
-      case _                                         => MergeStrategy.deduplicate
-    }
-
     def also(ss: Seq[Setting[_]]): Project            = p settings (ss: _*)
     def also(s: Setting[_], ss: Setting[_]*): Project = also(s +: ss.toSeq)
     def deps(ms: ModuleID*): Project                  = also(libraryDependencies ++= ms.toSeq)
     def root: Project                                 = p in file(".")
-    def testLogging: Project                          = p dependsOn LocalProject("logging") % "test->test"
-    def usesCommon: Project                           = p dependsOn LocalProject("common") % Both
-    def setup: Project                                = p dependsOn LocalProject("blueeyes") also commonSettings
-    def assemblyProject: Project                      = p.setup.testLogging also assemblySettings also (
-                       test in assembly :=  (),
-            assemblyJarName in assembly :=  "%s-assembly-%s.jar".format(name.value, "git describe".!!.trim),
-                     target in assembly <<= target,
-      assemblyMergeStrategy in assembly :=  assemblyMerger
-    )
-    def inBothScopes: ClasspathDependency = p % Both
+    def setup: Project                                = p also commonSettings
+    def setupAssembly: Project                        = setup also assemblyProjectSettings
+
+    def inBothScopes: ClasspathDependency = p % "compile->compile;test->test"
+    def inTestScope: ClasspathDependency  = p % "test->test"
   }
 }
