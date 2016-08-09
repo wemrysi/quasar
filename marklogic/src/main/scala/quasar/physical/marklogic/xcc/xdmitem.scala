7053a4bfe462ae3b2143c292da6813a3b8564ab3
Emrys Ingersoll <ingersoll@gmail.com>
2016-08-09 14:02:49 -0700

    Convert JSON XDM data to quasar.Data

---
+++ marklogic/src/main/scala/quasar/physical/marklogic/xcc/xdmitem.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2014–2016 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.physical.marklogic.xcc
+
+import quasar.Predef._
+import quasar.Data
+
+import com.marklogic.xcc.types._
+
+object xdmitem {
+  val toData: XdmItem => Option[Data] = {
+    case jitem: JsonItem => data.JsonParser.parseFromString(jitem.asString).toOption
+    case _               => None
+  }
+}
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,6 +7,7 @@ import sbt._, Keys._
 object Dependencies {
   private val argonautVersion   = "6.2-M3"
   private val http4sVersion     = "0.14.1a"
+  private val jawnVersion       = "0.8.4"
   private val monocleVersion    = "1.2.2"
   private val nettyVersion      = "4.1.3.Final"
   private val pathyVersion      = "0.2.1"
@@ -61,8 +62,9 @@ object Dependencies {
   def marklogic = Seq(
     "com.marklogic"  %  "java-client-api"   % "3.0.5",
     "com.marklogic"  %  "marklogic-xcc"     % "8.0.5",
-    "org.http4s"     %% "jawn-streamz"      % "0.8.1",
-    "org.spire-math" %% "jawn-argonaut"     % "0.8.4"
+    "org.spire-math" %% "jawn-argonaut"     % jawnVersion,
+    "org.spire-math" %% "jawn-parser"       % jawnVersion,
+    "org.http4s"     %% "jawn-streamz"      % "0.8.1"
   )
   def web = Seq(
     "ch.qos.logback"  % "logback-classic"     %      "1.1.7",
