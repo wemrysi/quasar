c174ca41e45f9bed593a6eb458443866f1752d08
Emrys Ingersoll <ingersoll@gmail.com>
2016-08-09 16:59:59 -0700

    Stub out the rest of XdmItem => Data conversion
    Include conversions for low-hanging primitive types.

--- marklogic/src/main/scala/quasar/physical/marklogic/xcc/xdmitem.scala
+++ marklogic/src/main/scala/quasar/physical/marklogic/xcc/xdmitem.scala
@@ -20,10 +20,47 @@ import quasar.Predef._
 import quasar.Data
 
 import com.marklogic.xcc.types._
+import scalaz._
 
 object xdmitem {
+  // TODO: What sort of error handling to we want?
   val toData: XdmItem => Option[Data] = {
-    case jitem: JsonItem => data.JsonParser.parseFromString(jitem.asString).toOption
-    case _               => None
+    case item: CtsBox                   => None
+    case item: CtsCircle                => None
+    case item: CtsPoint                 => None
+    case item: CtsPolygon               => None
+    // TODO: What is the difference between JSArray/Object and their *Node variants?
+    case item: JSArray                  => None
+    case item: JSObject                 => None
+    case item: JsonItem                 => data.JsonParser.parseFromString(item.asString).toOption
+    case item: XdmAttribute             => None
+    // TODO: Inefficient for large data as it must be buffered into memory
+    case item: XdmBinary                => Some(Data.Binary(ImmutableArray.fromArray(item.asBinaryData)))
+    case item: XdmComment               => None
+    case item: XdmDocument              => None
+    case item: XdmElement               => None
+    case item: XdmProcessingInstruction => None
+    case item: XdmText                  => None
+    case item: XSAnyURI                 => None
+    case item: XSBase64Binary           => Some(Data.Binary(ImmutableArray.fromArray(item.asBinaryData)))
+    case item: XSBoolean                => Some(Data.Bool(item.asPrimitiveBoolean))
+    case item: XSDate                   => None
+    case item: XSDateTime               => None
+    case item: XSDecimal                => Some(Data.Dec(item.asBigDecimal))
+    case item: XSDouble                 => Some(Data.Dec(item.asBigDecimal))
+    case item: XSDuration               => None
+    case item: XSFloat                  => Some(Data.Dec(item.asBigDecimal))
+    case item: XSGDay                   => None
+    case item: XSGMonth                 => None
+    case item: XSGMonthDay              => None
+    case item: XSGYear                  => None
+    case item: XSGYearMonth             => None
+    case item: XSHexBinary              => Some(Data.Binary(ImmutableArray.fromArray(item.asBinaryData)))
+    case item: XSInteger                => Some(Data.Int(item.asBigInteger))
+    case item: XSQName                  => None
+    case item: XSString                 => Some(Data.Str(item.asString))
+    case item: XSTime                   => None
+    case item: XSUntypedAtomic          => None
+    case _                              => None
   }
 }
