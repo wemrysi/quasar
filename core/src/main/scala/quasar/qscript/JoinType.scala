3b247d9db73859f5a62dbb7fe274dc5a7ecf16a6
Alissa Pajer <alissa.pajer@gmail.com>
2016-08-12 13:49:10 +0200

    Rearrange QScript files.

---
+++ core/src/main/scala/quasar/qscript/JoinType.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2014–2016 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.qscript
+
+import scalaz._
+
+sealed abstract class JoinType
+final case object Inner extends JoinType
+final case object FullOuter extends JoinType
+final case object LeftOuter extends JoinType
+final case object RightOuter extends JoinType
+
+object JoinType {
+  implicit val equal: Equal[JoinType] = Equal.equalRef
+  implicit val show: Show[JoinType] = Show.showFromToString
+}
