830688ba971d5addef25553dc42f77080fb5b3c6
Pawel Szulc <paul.szulc@gmail.com>
2016-08-12 13:00:08 +0200

    Refactor ReadFileSpec

--- sparkcore/src/test/scala/quasar/physical/sparkcore/fs/ReadFileSpec.scala
+++ sparkcore/src/test/scala/quasar/physical/sparkcore/fs/ReadFileSpec.scala
@@ -18,26 +18,22 @@ package quasar.physical.sparkcore.fs
 
 
 import quasar.Predef._
-import quasar.Data
 import quasar.fp.TaskRef
 import quasar.fp.numeric._
 import quasar.fp.free._
 import quasar.fs._
 import quasar.fs.ReadFile.ReadHandle
 import quasar.effect._
-// import quasar.physical.sparkcore.fs.local.readfile
+
+import java.io._
+import java.nio.file.{Files, Paths}
+import java.lang.System
 
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
-
-import java.io._
 import pathy.Path.posixCodec
-
 import scalaz._, Scalaz._, concurrent.Task
-
 import org.apache.spark._
-
-
 
 class ReadFileSpec extends Specification with ScalaCheck  {
 
@@ -50,62 +46,108 @@ class ReadFileSpec extends Specification with ScalaCheck  {
     "open - read chunk - close" in {
       // given
       import quasar.Data._
-      implicit val sc = newSc()
-
       val content = List(
         """{"login" : "john", "age" : 28}""",
         """{"login" : "kate", "age" : 31}"""
       )
-      val path: String = tempFile(content)
-      val aFile: AFile = sandboxAbs(posixCodec.parseAbsFile(path).get)
-      // when
-      readOneChunk(aFile).run.foldMap(inter).unsafePerformSync must beLike {
-        case \/-(results) =>
-          // then
-          results.size must be_==(2)
-          results must contain(Obj(ListMap("login" -> Str("john"), "age" -> Int(28))))
-          results must contain(Obj(ListMap("login" -> Str("kate"), "age" -> Int(31))))
+      val program = (f: AFile) => define { unsafe =>
+        for {
+          handle   <- unsafe.open(f, Natural(0).get, None)
+          readData <- unsafe.read(handle)
+          _        <- unsafe.close(handle).liftM[FileSystemErrT]
+        } yield readData
       }
-      sc.stop()
+
+      // when
+      withTempFile(createIt = Some(content)) { aFile =>
+        for {
+          sc <- newSc()
+          result <- execute(program(aFile), sc)
+        } yield {
+          result must beLike {
+            case \/-(results) =>
+              // then
+              results.size must be_==(2)
+              results must contain(Obj(ListMap("login" -> Str("john"), "age" -> Int(28))))
+              results must contain(Obj(ListMap("login" -> Str("kate"), "age" -> Int(31))))
+          }
+          sc.stop()
+        }
+      }
       ok
     }
   }
 
-  private def readOneChunk(f: AFile)
-    (implicit unsafe: ReadFile.Unsafe[ReadFile]):
-      FileSystemErrT[Free[ReadFile, ?], Vector[Data]] = for {
-    handle   <- unsafe.open(f, Natural(0).get, None)
-    readData <- unsafe.read(handle)
-    _        <- unsafe.close(handle).liftM[FileSystemErrT]
-  } yield readData
+  private def define[C]
+    (defined: ReadFile.Unsafe[ReadFile] => FileSystemErrT[Free[ReadFile, ?], C])
+    (implicit writeUnsafe: ReadFile.Unsafe[ReadFile])
+      : FileSystemErrT[Free[ReadFile, ?], C] =
+    defined(writeUnsafe)
 
-  private def run(implicit sc: SparkContext): Eff ~> Task = {
-    val genState = TaskRef(0L).unsafePerformSync
-    val kvsState = TaskRef(Map.empty[ReadHandle, SparkCursor]).unsafePerformSync
+  private def execute[C](program: FileSystemErrT[Free[ReadFile, ?], C], sc: SparkContext):
+      Task[FileSystemError \/ C] = interpreter(sc).flatMap(program.run.foldMap(_))
 
-    MonotonicSeq.fromTaskRef(genState) :+:
-    NaturalTransformation.refl[Task] :+:
-    KeyValueStore.fromTaskRef[ReadHandle, SparkCursor](kvsState) :+:
-    Read.constant[Task, SparkContext](sc)
+  private def interpreter(sc: SparkContext): Task[ReadFile ~> Task] = {
+
+    def innerInterpreter: Task[Eff ~> Task] =  {
+      (TaskRef(0L) |@| TaskRef(Map.empty[ReadHandle, SparkCursor])) { (genState, kvsState) =>
+        MonotonicSeq.fromTaskRef(genState) :+:
+        NaturalTransformation.refl[Task] :+:
+	KeyValueStore.fromTaskRef[ReadHandle, SparkCursor](kvsState) :+:
+        Read.constant[Task, SparkContext](sc)
+      }
+    }
+
+    innerInterpreter.map { inner =>
+      readfile.interpret[Eff](local.readfile.input[Eff]) andThen foldMapNT[Eff, Task](inner)
+    }
   }
-    
 
-  private def inter(implicit sc: SparkContext): ReadFile ~> Task =
-    readfile.interpret[Eff](local.readfile.input[Eff]) andThen foldMapNT[Eff, Task](run)
-
-  private def newSc(): SparkContext = {
+  private def newSc(): Task[SparkContext] = Task.delay {
     val config = new SparkConf().setMaster("local[*]").setAppName(this.getClass().getName())
     new SparkContext(config)
   }
 
-  private def tempFile(content: Seq[String]): String = {
-    val file = File.createTempFile(scala.util.Random.nextInt().toString, ".tmp")
-    val writer = new PrintWriter(file)
-    content.foreach {
-      line => writer.write(line + "\n")
+  type Content = List[String]
+
+  private def withTempFile[C](createIt: Option[Content] = None)(run: AFile => Task[C]): C = {
+
+    def genTempFilePath: Task[AFile] = Task.delay {
+      val path = System.getProperty("java.io.tmpdir") +
+      "/" + scala.util.Random.nextInt().toString + ".tmp"
+      sandboxAbs(posixCodec.parseAbsFile(path).get)
     }
-    writer.flush()
-    writer.close()
-    file.getAbsolutePath()
+
+    def createFile(filePath: AFile): Task[Unit] = Task.delay {
+      createIt.foreach { content =>
+        val file = toNioPath(filePath).toFile()
+        val writer = new PrintWriter(file)
+        content.foreach {
+          line => writer.write(line + "\n")
+        }
+        writer.flush()
+        writer.close()
+      }
+    }
+
+    def deleteFile(file: AFile): Task[Unit] = Task.delay {
+      Files.delete(Paths.get(posixCodec.unsafePrintPath(file)))
+    }
+
+    val execution: Task[C] = for {
+      filePath <- genTempFilePath
+      _ <- createFile(filePath)
+      result <- run(filePath).onFinish {
+        _ => deleteFile(filePath)
+      }
+    } yield result
+
+    execution.unsafePerformSync
   }
+
+  private def toNioPath(path: APath) =
+    Paths.get(posixCodec.unsafePrintPath(path))
+
+
+
 }
