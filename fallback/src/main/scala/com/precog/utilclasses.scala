fe598876fa4dd754372fc0fb82e65ac310d72e63
Paul Phillips <paulp@improving.org>
2016-08-16 12:53:17 -0600

    No VectorClock.

--- fallback/src/main/scala/com/precog/utilclasses.scala
+++ fallback/src/main/scala/com/precog/utilclasses.scala
@@ -1,7 +1,6 @@
 package com.precog.util
 
-import blueeyes._, json._, serialization._
-import DefaultSerialization._, Extractor._
+import blueeyes._
 import scala.{ collection => sc }
 import scalaz._, Ordering._
 import scala.collection.generic.CanBuildFrom
@@ -231,52 +230,3 @@ final class RingDeque[@specialized(Boolean, Int, Long, Double, Float, Short) A: 
     back = rotate(back, delta)
   }
 }
-
-case class VectorClock(map: Map[Int, Int]) {
-  def get(producerId: Int): Option[Int] = map.get(producerId)
-
-  def update(producerId: Int, sequenceId: Int): VectorClock =
-    if (map.get(producerId) forall { _ <= sequenceId }) {
-      VectorClock(map + (producerId -> sequenceId))
-    } else {
-      this
-    }
-
-  def isDominatedBy(other: VectorClock): Boolean = map forall {
-    case (prodId, maxSeqId) => other.get(prodId).forall(_ >= maxSeqId)
-  }
-}
-
-trait VectorClockSerialization {
-  implicit val VectorClockDecomposer: Decomposer[VectorClock] = new Decomposer[VectorClock] {
-    override def decompose(clock: VectorClock): JValue = clock.map.serialize
-  }
-
-  implicit val VectorClockExtractor: Extractor[VectorClock] = new Extractor[VectorClock] {
-    override def validated(obj: JValue): Validation[Error, VectorClock] =
-      (obj.validated[Map[Int, Int]]).map(VectorClock(_))
-  }
-}
-
-object VectorClock extends VectorClockSerialization {
-  def empty = apply(Map.empty)
-
-  implicit object order extends scalaz.Order[VectorClock] {
-    def order(c1: VectorClock, c2: VectorClock) =
-      if (c2.isDominatedBy(c1)) {
-        if (c1.isDominatedBy(c2)) EQ else GT
-      } else {
-        LT
-      }
-  }
-
-  // Computes the maximal merge of two clocks
-  implicit object semigroup extends Semigroup[VectorClock] {
-    def append(c1: VectorClock, c2: => VectorClock) = {
-      c2.map.foldLeft(c1) {
-        case (acc, (producerId, sequenceId)) => acc.update(producerId, sequenceId)
-      }
-    }
-  }
-}
-
