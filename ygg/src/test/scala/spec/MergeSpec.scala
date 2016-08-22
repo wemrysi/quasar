f5566ff8d100ca57bc694582c31693e72c7fcfd8
Paul Phillips <paulp@improving.org>
2016-08-22 14:41:23 -0600

    Use json macro.

--- ygg/src/test/scala/spec/MergeSpec.scala
+++ ygg/src/test/scala/spec/MergeSpec.scala
@@ -39,24 +39,22 @@ class MergeSpec extends quasar.Qspec with ColumnarTableModuleTestSupport with Ta
         {"key":[5908438637678328473],"value":{"a":3,"b":7}}
       """.toStream)
 
-      val barJson = """
-        | {"key":[5908438637678328576],"value":{"a":-1,"c":8,"b":-1}}
-        | {"key":[5908438637678328577],"value":{"a":1,"c":9,"b":-1}}
-        | {"key":[5908438637678328578],"value":{"a":-1,"c":10,"b":6}}
-        | {"key":[5908438637678328579],"value":{"a":3,"c":11,"b":7}}
-        | {"key":[5908438637678328580],"value":{"a":0,"c":12,"b":-1}}
-        | {"key":[5908438637678328581],"value":{"a":0,"c":13,"b":-1}}
-        | """.stripMargin
-      val bar     = fromJson(JParser.parseManyFromString(barJson).valueOr(throw _).toStream)
+      val bar = fromJson(jsonMany"""
+        {"key":[5908438637678328576],"value":{"a":-1,"c":8,"b":-1}}
+        {"key":[5908438637678328577],"value":{"a":1,"c":9,"b":-1}}
+        {"key":[5908438637678328578],"value":{"a":-1,"c":10,"b":6}}
+        {"key":[5908438637678328579],"value":{"a":3,"c":11,"b":7}}
+        {"key":[5908438637678328580],"value":{"a":0,"c":12,"b":-1}}
+        {"key":[5908438637678328581],"value":{"a":0,"c":13,"b":-1}}
+      """.toStream)
 
-      val resultJson0 = """
-        | {"key":[5908438637678328470,5908438637678328580],"value":{"b":4,"c":12,"a":0,"fa":{"b":4,"a":0}}}
-        | {"key":[5908438637678328470,5908438637678328581],"value":{"b":4,"c":13,"a":0,"fa":{"b":4,"a":0}}}
-        | {"key":[5908438637678328471,5908438637678328577],"value":{"b":5,"c":9,"a":1,"fa":{"b":5,"a":1}}}
-        | {"key":[5908438637678328472,5908438637678328578],"value":{"b":6,"c":10,"a":2,"fa":{"b":6,"a":2}}}
-        | {"key":[5908438637678328473,5908438637678328579],"value":{"b":7,"c":11,"a":3,"fa":{"b":7,"a":3}}}
-        | """.stripMargin
-      val resultJson  = JParser.parseManyFromString(resultJson0).valueOr(throw _)
+      val resultJson = jsonMany"""
+        {"key":[5908438637678328470,5908438637678328580],"value":{"b":4,"c":12,"a":0,"fa":{"b":4,"a":0}}}
+        {"key":[5908438637678328470,5908438637678328581],"value":{"b":4,"c":13,"a":0,"fa":{"b":4,"a":0}}}
+        {"key":[5908438637678328471,5908438637678328577],"value":{"b":5,"c":9,"a":1,"fa":{"b":5,"a":1}}}
+        {"key":[5908438637678328472,5908438637678328578],"value":{"b":6,"c":10,"a":2,"fa":{"b":6,"a":2}}}
+        {"key":[5908438637678328473,5908438637678328579],"value":{"b":7,"c":11,"a":3,"fa":{"b":7,"a":3}}}
+      """
 
       val keyField   = CPathField("key")
       val valueField = CPathField("value")
@@ -94,29 +92,27 @@ class MergeSpec extends quasar.Qspec with ColumnarTableModuleTestSupport with Ta
           GroupingSpec.Intersection)
 
       def evaluator(key: RValue, partition: GroupId => Need[Table]) = {
-        val K0 = RValue.fromJValue(JParser.parseUnsafe("""{"1":0,"2":4}"""))
-        val K1 = RValue.fromJValue(JParser.parseUnsafe("""{"1":1,"2":5}"""))
-        val K2 = RValue.fromJValue(JParser.parseUnsafe("""{"1":2,"2":6}"""))
-        val K3 = RValue.fromJValue(JParser.parseUnsafe("""{"1":3,"2":7}"""))
+        val K0 = RValue.fromJValue(json"""{"1":0,"2":4}""")
+        val K1 = RValue.fromJValue(json"""{"1":1,"2":5}""")
+        val K2 = RValue.fromJValue(json"""{"1":2,"2":6}""")
+        val K3 = RValue.fromJValue(json"""{"1":3,"2":7}""")
 
-        val r0Json = """
-          | {"key":[5908438637678328470,5908438637678328580],"value":{"b":4,"c":12,"a":0,"fa":{"b":4,"a":0}}}
-          | {"key":[5908438637678328470,5908438637678328581],"value":{"b":4,"c":13,"a":0,"fa":{"b":4,"a":0}}}
-          | """.stripMargin
-        val r1Json = """
-          | {"key":[5908438637678328471,5908438637678328577],"value":{"b":5,"c":9,"a":1,"fa":{"b":5,"a":1}}}
-          """.stripMargin
-        val r2Json = """
-          | {"key":[5908438637678328472,5908438637678328578],"value":{"b":6,"c":10,"a":2,"fa":{"b":6,"a":2}}}
-          | """.stripMargin
-        val r3Json = """
-          | {"key":[5908438637678328473,5908438637678328579],"value":{"b":7,"c":11,"a":3,"fa":{"b":7,"a":3}}}
-          | """.stripMargin
+        val r0 = fromJson(jsonMany"""
+          {"key":[5908438637678328470,5908438637678328580],"value":{"b":4,"c":12,"a":0,"fa":{"b":4,"a":0}}}
+          {"key":[5908438637678328470,5908438637678328581],"value":{"b":4,"c":13,"a":0,"fa":{"b":4,"a":0}}}
+        """.toStream)
 
-        val r0 = fromJson(JParser.parseManyFromString(r0Json).valueOr(throw _).toStream)
-        val r1 = fromJson(JParser.parseManyFromString(r1Json).valueOr(throw _).toStream)
-        val r2 = fromJson(JParser.parseManyFromString(r2Json).valueOr(throw _).toStream)
-        val r3 = fromJson(JParser.parseManyFromString(r3Json).valueOr(throw _).toStream)
+        val r1 = fromJson(jsonMany"""
+          {"key":[5908438637678328471,5908438637678328577],"value":{"b":5,"c":9,"a":1,"fa":{"b":5,"a":1}}}
+        """.toStream)
+
+        val r2 = fromJson(jsonMany"""
+          {"key":[5908438637678328472,5908438637678328578],"value":{"b":6,"c":10,"a":2,"fa":{"b":6,"a":2}}}
+        """.toStream)
+
+        val r3 = fromJson(jsonMany"""
+          {"key":[5908438637678328473,5908438637678328579],"value":{"b":7,"c":11,"a":3,"fa":{"b":7,"a":3}}}
+        """.toStream)
 
         Need {
           key match {
@@ -134,27 +130,25 @@ class MergeSpec extends quasar.Qspec with ColumnarTableModuleTestSupport with Ta
     }
 
     "execute the medals query without a cross" in {
-      val medalsJson = """
-        | {"key":[5908438637678314371],"value":{"Edition":"2000","Gender":"Men"}}
-        | {"key":[5908438637678314372],"value":{"Edition":"1996","Gender":"Men"}}
-        | {"key":[5908438637678314373],"value":{"Edition":"2008","Gender":"Men"}}
-        | {"key":[5908438637678314374],"value":{"Edition":"2004","Gender":"Women"}}
-        | {"key":[5908438637678314375],"value":{"Edition":"2000","Gender":"Women"}}
-        | {"key":[5908438637678314376],"value":{"Edition":"1996","Gender":"Women"}}
-        | {"key":[5908438637678314377],"value":{"Edition":"2008","Gender":"Men"}}
-        | {"key":[5908438637678314378],"value":{"Edition":"2004","Gender":"Men"}}
-        | {"key":[5908438637678314379],"value":{"Edition":"1996","Gender":"Men"}}
-        | {"key":[5908438637678314380],"value":{"Edition":"2008","Gender":"Women"}}
-        | """.stripMargin
-      val medals     = fromJson(JParser.parseManyFromString(medalsJson).valueOr(throw _).toStream)
+      val medals = fromJson(jsonMany"""
+        {"key":[5908438637678314371],"value":{"Edition":"2000","Gender":"Men"}}
+        {"key":[5908438637678314372],"value":{"Edition":"1996","Gender":"Men"}}
+        {"key":[5908438637678314373],"value":{"Edition":"2008","Gender":"Men"}}
+        {"key":[5908438637678314374],"value":{"Edition":"2004","Gender":"Women"}}
+        {"key":[5908438637678314375],"value":{"Edition":"2000","Gender":"Women"}}
+        {"key":[5908438637678314376],"value":{"Edition":"1996","Gender":"Women"}}
+        {"key":[5908438637678314377],"value":{"Edition":"2008","Gender":"Men"}}
+        {"key":[5908438637678314378],"value":{"Edition":"2004","Gender":"Men"}}
+        {"key":[5908438637678314379],"value":{"Edition":"1996","Gender":"Men"}}
+        {"key":[5908438637678314380],"value":{"Edition":"2008","Gender":"Women"}}
+      """.toStream)
 
-      val resultJson0 = """
-        | {"key":[],"value":{"year":"1996","ratio":139.0}}
-        | {"key":[],"value":{"year":"2000","ratio":126.0}}
-        | {"key":[],"value":{"year":"2004","ratio":122.0}}
-        | {"key":[],"value":{"year":"2008","ratio":119.0}}
-        | """.stripMargin
-      val resultJson  = JParser.parseManyFromString(resultJson0).valueOr(throw _)
+      val resultJson = jsonMany"""
+        {"key":[],"value":{"year":"1996","ratio":139.0}}
+        {"key":[],"value":{"year":"2000","ratio":126.0}}
+        {"key":[],"value":{"year":"2004","ratio":122.0}}
+        {"key":[],"value":{"year":"2008","ratio":119.0}}
+      """.toStream
 
       val keyField     = CPathField("key")
       val valueField   = CPathField("value")
@@ -203,28 +197,15 @@ class MergeSpec extends quasar.Qspec with ColumnarTableModuleTestSupport with Ta
           GroupingSpec.Intersection)
 
       def evaluator(key: RValue, partition: GroupId => Need[Table]) = {
-        val K0 = RValue.fromJValue(JParser.parseUnsafe("""{"1":"1996","extra0":true,"extra1":true}"""))
-        val K1 = RValue.fromJValue(JParser.parseUnsafe("""{"1":"2000","extra0":true,"extra1":true}"""))
-        val K2 = RValue.fromJValue(JParser.parseUnsafe("""{"1":"2004","extra0":true,"extra1":true}"""))
-        val K3 = RValue.fromJValue(JParser.parseUnsafe("""{"1":"2008","extra0":true,"extra1":true}"""))
+        val K0 = RValue.fromJValue(json"""{"1":"1996","extra0":true,"extra1":true}""")
+        val K1 = RValue.fromJValue(json"""{"1":"2000","extra0":true,"extra1":true}""")
+        val K2 = RValue.fromJValue(json"""{"1":"2004","extra0":true,"extra1":true}""")
+        val K3 = RValue.fromJValue(json"""{"1":"2008","extra0":true,"extra1":true}""")
 
-        val r0Json = """
-          | {"key":[],"value":{"year":"1996","ratio":139.0}}
-          | """.stripMargin
-        val r1Json = """
-        | {"key":[],"value":{"year":"2000","ratio":126.0}}
-          | """.stripMargin
-        val r2Json = """
-          | {"key":[],"value":{"year":"2004","ratio":122.0}}
-          | """.stripMargin
-        val r3Json = """
-          | {"key":[],"value":{"year":"2008","ratio":119.0}}
-          | """.stripMargin
-
-        val r0 = fromJson(JParser.parseManyFromString(r0Json).valueOr(throw _).toStream)
-        val r1 = fromJson(JParser.parseManyFromString(r1Json).valueOr(throw _).toStream)
-        val r2 = fromJson(JParser.parseManyFromString(r2Json).valueOr(throw _).toStream)
-        val r3 = fromJson(JParser.parseManyFromString(r3Json).valueOr(throw _).toStream)
+        val r0 = fromJson(jsonMany"""{"key":[],"value":{"year":"1996","ratio":139.0}}""".toStream)
+        val r1 = fromJson(jsonMany"""{"key":[],"value":{"year":"2000","ratio":126.0}}""".toStream)
+        val r2 = fromJson(jsonMany"""{"key":[],"value":{"year":"2004","ratio":122.0}}""".toStream)
+        val r3 = fromJson(jsonMany"""{"key":[],"value":{"year":"2008","ratio":119.0}}""".toStream)
 
         Need {
           key match {
