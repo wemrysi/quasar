035004cee5be5e56ff327886ef5b7e7cf2579ded
Paul Phillips <paulp@improving.org>
2016-08-25 17:35:53 -0600

    Trim Path.

--- ygg/src/main/scala/ygg/table/Path.scala
+++ ygg/src/main/scala/ygg/table/Path.scala
@@ -1,55 +1,30 @@
 package ygg.table
 
-import scalaz._, Scalaz._
+import ygg.common._
 
-class Path private (val elements: String*) {
-  def components: List[String] = elements.toList
-  val path: String             = elements.mkString("/", "/", "/").replaceAll("/+", "/")
-  val length: Int              = elements.length
+final case class Path private (components: Vector[String]) {
+  def isEmpty                 = components.isEmpty
+  def path: String            = (components mkString "/").replaceAll("/+", "/")
+  def length: Int             = components.length
+  def parent: Path            = if (isEmpty) this else Path(components.init)
+  def ancestors: Vector[Path] = if (isEmpty) vec() else parent +: parent.ancestors
 
-  lazy val parent: Option[Path] = elements.size match {
-    case 0 => None
-    case 1 => Some(Path.Root)
-    case _ => Some(new Path(elements.init: _*))
-  }
+  def isEqualOrParentOf(that: Path) = that.components startsWith this.components
 
-  lazy val ancestors: List[Path] = {
-    val parentList = parent.toList
+  def mapSegments(f: Vec[String] => Vec[String]): Path = Path(f(components))
 
-    parentList ++ parentList.flatMap(_.ancestors)
-  }
-
-  def /(that: Path)               = new Path(elements ++ that.elements: _*)
-  def -(that: Path): Option[Path] = elements.startsWith(that.elements).option(new Path(elements.drop(that.elements.length): _*))
-
-  def isEqualOrParentOf(that: Path) = that.elements.startsWith(this.elements)
-
-  def isChildOf(that: Path) = elements.startsWith(that.elements) && length > that.length
-
-  def rollups(depth: Int): List[Path] = this :: ancestors.take(depth)
-
-  def urlEncode: Path = new Path(elements.map(java.net.URLEncoder.encode(_, "UTF-8")): _*)
-
-  def prefix: Option[Path] = elements.nonEmpty.option(Path(components.init))
-
-  override def equals(that: Any) = that match {
-    case Path(`path`) => true
-    case _            => false
-  }
-
-  override def hashCode = path.hashCode
-
-  override def toString = path
+  def /(that: Path)                     = mapSegments(_ ++ that.components)
+  def rollups(depth: Int): Vector[Path] = this +: (ancestors take depth)
+  def urlEncode: Path                   = mapSegments(_ map (c => java.net.URLEncoder.encode(c, "UTF-8")))
+  def prefix: Path                      = if (isEmpty) this else mapSegments(_.init)
+  override def toString                 = path
 }
 
 object Path {
-  val Root = new Path()
+  val Root = new Path(vec())
 
   private def cleanPath(string: String): String = string.replaceAll("^/|/$", "").replaceAll("/+", "/")
 
-  def apply(path: String): Path = new Path(cleanPath(path).split("/").filterNot(_.trim.isEmpty): _*)
-
-  def apply(elements: List[String]): Path = apply(elements.mkString("/"))
-
-  def unapply(path: Path): Option[String] = Some(path.path)
+  def apply(path: String): Path       = new Path(cleanPath(path).split("/").filterNot(_.trim.isEmpty).toVector)
+  def apply(elems: Seq[String]): Path = apply(elems mkString "/")
 }
