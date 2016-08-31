d750dac22ef7a8a40f6cec5c79af8bb334204080
Paul Phillips <paulp@improving.org>
2016-08-30 19:16:12 -0600

    Recover travis.

---
+++ bin/run-travis.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+
+set -euo pipefail
+unset SBT_OPTS JVM_OPTS JDK_HOME JAVA_HOME
+: ${TRAVIS_SCALA_VERSION:=2.11.8}
+: ${SBT_TARGET:=test}
+
+ANSI_CLEAR="\033[0K"
+
+travis_fold() {
+  local action="$1"
+  local name="$2"
+  echo -en "travis_fold:${action}:${name}\r${ANSI_CLEAR}"
+}
+runSbt () {
+  local foldid="$1" && shift
+  travis_fold start $foldid
+  ./sbt ++$TRAVIS_SCALA_VERSION -batch "$@" | ts '%H:%M:%.S'
+  travis_fold end $foldid
+}
+
+runSbt "sbt-update" -v update
+runSbt "sbt-test" -v "$SBT_TARGET"
