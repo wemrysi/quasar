198b9f7e98a249ce6fd8085a6af3bf32029c17c4
Alissa Pajer <alissa.pajer@gmail.com>
2016-09-12 15:15:38 -0600

    Remove unnecessary type annotations.

--- core/src/main/scala/quasar/qscript/MapFunc.scala
+++ core/src/main/scala/quasar/qscript/MapFunc.scala
@@ -176,37 +176,31 @@ object MapFunc {
             Embed(CoEnv(\/-(Constant(Embed(ejson.Common(ejson.Bool(v1))))))),
             Embed(CoEnv(\/-(Constant(Embed(ejson.Common(ejson.Bool(v2)))))))) =>
           CoEnv[A, MapFunc[T, ?], T[CoEnv[A, MapFunc[T, ?], ?]]](
-            Constant[T, T[CoEnv[A, MapFunc[T, ?], ?]]](
-              CommonEJson.inj(ejson.Bool[T[EJson]](v1 && v2)).embed).right).some
+            Constant(CommonEJson.inj(ejson.Bool[T[EJson]](v1 && v2)).embed).right).some
 
         case Or(
             Embed(CoEnv(\/-(Constant(Embed(ejson.Common(ejson.Bool(v1))))))),
             Embed(CoEnv(\/-(Constant(Embed(ejson.Common(ejson.Bool(v2)))))))) =>
           CoEnv[A, MapFunc[T, ?], T[CoEnv[A, MapFunc[T, ?], ?]]](
-            Constant[T, T[CoEnv[A, MapFunc[T, ?], ?]]](
-              CommonEJson.inj(ejson.Bool[T[EJson]](v1 || v2)).embed).right).some
+            Constant(CommonEJson.inj(ejson.Bool[T[EJson]](v1 || v2)).embed).right).some
 
         case Not(Embed(CoEnv(\/-(Constant(Embed(ejson.Common(ejson.Bool(v1)))))))) =>
           CoEnv[A, MapFunc[T, ?], T[CoEnv[A, MapFunc[T, ?], ?]]](
-            Constant[T, T[CoEnv[A, MapFunc[T, ?], ?]]](
-              CommonEJson.inj(ejson.Bool[T[EJson]](!v1)).embed).right).some
+            Constant(CommonEJson.inj(ejson.Bool[T[EJson]](!v1)).embed).right).some
 
         // string
         case Lower(Embed(CoEnv(\/-(Constant(Embed(ejson.Common(ejson.Str(v1)))))))) =>
           CoEnv[A, MapFunc[T, ?], T[CoEnv[A, MapFunc[T, ?], ?]]](
-            Constant[T, T[CoEnv[A, MapFunc[T, ?], ?]]](
-              CommonEJson.inj(ejson.Str[T[EJson]](v1.toLowerCase)).embed).right).some
+            Constant(CommonEJson.inj(ejson.Str[T[EJson]](v1.toLowerCase)).embed).right).some
 
         case Upper(Embed(CoEnv(\/-(Constant(Embed(ejson.Common(ejson.Str(v1)))))))) =>
           CoEnv[A, MapFunc[T, ?], T[CoEnv[A, MapFunc[T, ?], ?]]](
-            Constant[T, T[CoEnv[A, MapFunc[T, ?], ?]]](
-              CommonEJson.inj(ejson.Str[T[EJson]](v1.toUpperCase)).embed).right).some
+            Constant(CommonEJson.inj(ejson.Str[T[EJson]](v1.toUpperCase)).embed).right).some
 
         // structural
         case MakeArray(Embed(CoEnv(\/-(Constant(v1))))) =>
           CoEnv[A, MapFunc[T, ?], T[CoEnv[A, MapFunc[T, ?], ?]]](
-            Constant[T, T[CoEnv[A, MapFunc[T, ?], ?]]](
-              CommonEJson.inj(ejson.Arr(List(v1))).embed).right).some
+            Constant(CommonEJson.inj(ejson.Arr[T[EJson]](List(v1))).embed).right).some
 
         case _ => None
       })
