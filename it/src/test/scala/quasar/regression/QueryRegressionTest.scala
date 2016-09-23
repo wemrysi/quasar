c2017e428b6983aea83ccc6f942e2c1ce7fdca7f
Emrys Ingersoll <ingersoll@gmail.com>
2016-09-23 18:20:13 -0500

    More informative error message when regression test data fails to load.

--- it/src/test/scala/quasar/regression/QueryRegressionTest.scala
+++ it/src/test/scala/quasar/regression/QueryRegressionTest.scala
@@ -204,7 +204,11 @@ abstract class QueryRegressionTest[S[_]](
       testData(dataLoc).chunk(500).translate(injectTask)
     ).translate[Task](throwFsError.compose[FsErr](runT(run)))
 
-    throwFsError(EitherT(load.take(1).runLast map (_ <\/ (()))))
+    throwFsError(EitherT(load.take(1).runLast map (_ <\/ (())))) handleWith {
+      case err =>
+        val msg = s"Failed loading test data '${posixCodec.printPath(dataLoc)}': ${err.getMessage}"
+        Task.fail(new RuntimeException(msg, err))
+    }
   }
 
   /** Returns a stream of `Data` representing the lines of the given data file. */
