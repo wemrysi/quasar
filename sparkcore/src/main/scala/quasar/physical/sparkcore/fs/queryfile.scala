414172bcff31d0854da9ed5e5de32ac02c240afc
Pawel Szulc <paul.szulc@gmail.com>
2016-09-26 21:19:49 +0200

    Fix bug: use calculated rddState

--- sparkcore/src/main/scala/quasar/physical/sparkcore/fs/queryfile.scala
+++ sparkcore/src/main/scala/quasar/physical/sparkcore/fs/queryfile.scala
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2014–2016 SlamData Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package quasar.physical.sparkcore.fs
 
 import quasar.Predef._
@@ -172,7 +156,7 @@ object queryfile {
               .map(_._1).collect.toVector
           }).into[S].liftM[FileSystemErrT]
           rddState = if(collected.isEmpty) RddState(None, 0) else RddState(Some(rdd), p + step)
-          _ <- kvs.put(h, RddState(None, 0)).liftM[FileSystemErrT]
+          _ <- kvs.put(h, rddState).liftM[FileSystemErrT]
         } yield(collected)
     }.run
   }
