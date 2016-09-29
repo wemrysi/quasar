fdf1c1332d4655b791758bd19f595a5a73f18051
Paul Phillips <paulp@improving.org>
2016-09-29 10:55:48 -0700

    Cleanups with T[_[_]] types and lambdas.
    Don't define classes inside of package objects if it can be avoided.
    There is a lot of accidental complexity there.
    
    Use polymorphic lambdas more frequently.
    
    Adapt SimplifyProjection methods similarly to Normalize.
    
    Eliminate more construction boilerplate.

--- connector/src/main/scala/quasar/qscript/TTypes.scala
+++ connector/src/main/scala/quasar/qscript/TTypes.scala
@@ -40,10 +40,62 @@ trait TTypes[T[_[_]]] {
 
 object TTypes {
   def normalizable[T[_[_]] : Recursive : Corecursive : EqualT : ShowT] = new NormalizableT[T]
+  def simplifiableProjection[T[_[_]]]                                  = new SimplifiableProjectionT[T]
+}
+
+class SimplifiableProjectionT[T[_[_]]] extends TTypes[T] {
+  import SimplifyProjection._
+
+  private lazy val simplify: EndoK[QScriptTotal] = simplifyQScriptTotal[T].simplifyProjection
+  private def applyToBranch(branch: FreeQS[T]): FreeQS[T] = branch mapSuspension simplify
+
+  def ProjectBucket[G[_]](implicit QC: QScriptCore :<: G) = make(
+    λ[ProjectBucket ~> G] {
+      case BucketField(src, value, field) => QC inj Map(src, Free roll MapFuncs.ProjectField(value, field))
+      case BucketIndex(src, value, index) => QC inj Map(src, Free roll MapFuncs.ProjectIndex(value, index))
+    }
+  )
+
+  def QScriptCore[G[_]](implicit QC: QScriptCore :<: G) = make(
+    λ[QScriptCore ~> G](fa =>
+      QC inj (fa match {
+        case Union(src, lb, rb) => Union(src, applyToBranch(lb), applyToBranch(rb))
+        case Drop(src, lb, rb)  => Drop(src, applyToBranch(lb), applyToBranch(rb))
+        case Take(src, lb, rb)  => Take(src, applyToBranch(lb), applyToBranch(rb))
+        case _                  => fa
+      })
+    )
+  )
+  def ThetaJoin[G[_]](implicit TJ: ThetaJoin :<: G) = make(
+    λ[ThetaJoin ~> G](tj =>
+      TJ inj quasar.qscript.ThetaJoin(
+        tj.src,
+        applyToBranch(tj.lBranch),
+        applyToBranch(tj.rBranch),
+        tj.on,
+        tj.f,
+        tj.combine
+      )
+    )
+  )
+  def EquiJoin[G[_]](implicit EJ: EquiJoin :<: G) = make(
+    λ[EquiJoin ~> G](ej =>
+      EJ inj quasar.qscript.EquiJoin(
+        ej.src,
+        applyToBranch(ej.lBranch),
+        applyToBranch(ej.rBranch),
+        ej.lKey,
+        ej.rKey,
+        ej.f,
+        ej.combine
+      )
+    )
+  )
 }
 
 // ShowT is needed for debugging
 class NormalizableT[T[_[_]] : Recursive : Corecursive : EqualT : ShowT] extends TTypes[T] {
+  import Normalizable._
   lazy val opt = new Optimize[T]
 
   def freeTC(free: FreeQS[T]): FreeQS[T] = {
@@ -57,8 +109,8 @@ class NormalizableT[T[_[_]] : Recursive : Corecursive : EqualT : ShowT] extends 
     )
   }
 
-  def EquiJoin = new Normalizable[EquiJoin] {
-    val normalize = λ[EndoK[EquiJoin]](ej =>
+  def EquiJoin = make(
+    λ[EndoK[EquiJoin]](ej =>
       quasar.qscript.EquiJoin(
         ej.src,
         freeTC(ej.lBranch),
@@ -66,49 +118,47 @@ class NormalizableT[T[_[_]] : Recursive : Corecursive : EqualT : ShowT] extends 
         freeMF(ej.lKey),
         freeMF(ej.rKey),
         ej.f,
-        freeMF(ej.combine))
+        freeMF(ej.combine)
+      )
     )
-  }
-  def ThetaJoin = new Normalizable[ThetaJoin] {
-    val normalize = λ[EndoK[ThetaJoin]](tj =>
+  )
+  def ThetaJoin = make(
+    λ[EndoK[ThetaJoin]](tj =>
       quasar.qscript.ThetaJoin(
         tj.src,
         freeTC(tj.lBranch),
         freeTC(tj.rBranch),
         freeMF(tj.on),
         tj.f,
-        freeMF(tj.combine))
+        freeMF(tj.combine)
+      )
     )
-  }
+  )
 
-  def QScriptCore = new Normalizable[QScriptCore] {
-    val normalize = λ[EndoK[QScriptCore]] {
+  def QScriptCore = {
+    // NB: all single-bucket reductions should reduce on `null`
+    def normalizeBucket(bucket: FreeMap[T]): FreeMap[T] = bucket.resume.fold({
+      case MapFuncs.Constant(_) => MapFuncs.NullLit[T, Hole]()
+      case _                    => bucket
+    }, κ(bucket))
+
+    make(λ[EndoK[QScriptCore]] {
       case Map(src, f)                           => Map(src, freeMF(f))
       case LeftShift(src, s, r)                  => LeftShift(src, freeMF(s), freeMF(r))
-      case Reduce(src, bucket, reducers, repair) =>
-        val normBuck = freeMF(bucket)
-        Reduce(
-          src,
-          // NB: all single-bucket reductions should reduce on `null`
-          normBuck.resume.fold({
-            case MapFuncs.Constant(_) => MapFuncs.NullLit[T, Hole]()
-            case _                    => normBuck
-          }, κ(normBuck)),
-          reducers.map(_.map(freeMF(_))),
-          freeMF(repair))
-      case Sort(src, bucket, order) => Sort(src, freeMF(bucket), order.map(_.leftMap(freeMF(_))))
-      case Union(src, l, r)         => Union(src, freeTC(l), freeTC(r))
-      case Filter(src, f)           => Filter(src, freeMF(f))
-      case Take(src, from, count)   => Take(src, freeTC(from), freeTC(count))
-      case Drop(src, from, count)   => Drop(src, freeTC(from), freeTC(count))
-      case Unreferenced()           => Unreferenced()
-    }
+      case Reduce(src, bucket, reducers, repair) => Reduce(src, normalizeBucket(freeMF(bucket)), reducers map (_ map freeMF), freeMF(repair))
+      case Sort(src, bucket, order)              => Sort(src, freeMF(bucket), order map (_ leftMap freeMF))
+      case Union(src, l, r)                      => Union(src, freeTC(l), freeTC(r))
+      case Filter(src, f)                        => Filter(src, freeMF(f))
+      case Take(src, from, count)                => Take(src, freeTC(from), freeTC(count))
+      case Drop(src, from, count)                => Drop(src, freeTC(from), freeTC(count))
+      case Unreferenced()                        => Unreferenced()
+    })
   }
 
-  def ProjectBucket = new Normalizable[ProjectBucket] {
-    val normalize = λ[EndoK[ProjectBucket]] {
+  def ProjectBucket = make(
+    λ[EndoK[ProjectBucket]] {
       case BucketField(a, v, f) => BucketField(a, freeMF(v), freeMF(f))
       case BucketIndex(a, v, i) => BucketField(a, freeMF(v), freeMF(i))
     }
-  }
+  )
 }
