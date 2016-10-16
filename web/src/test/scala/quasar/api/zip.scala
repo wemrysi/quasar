f8d30b849f052a4a3d8925b601ebec3ca8aedaa8
Moss Prescott <moss@theprescotts.com>
2016-10-16 15:58:40 -0400

    capture effectful generation of random data in Task
    And avoid allocating a java.util.Random instance for each byte.

--- web/src/test/scala/quasar/api/zip.scala
+++ web/src/test/scala/quasar/api/zip.scala
@@ -18,6 +18,7 @@ package quasar.api
 
 import scala.Predef.$conforms
 import quasar.fp.numeric._
+import quasar.fp.ski._
 
 import org.scalacheck.Arbitrary
 import quasar.Predef._
@@ -40,69 +41,75 @@ import eu.timepit.refined.auto._
 import eu.timepit.refined.auto._
 
 class ZipSpecs extends quasar.Qspec {
+  import Zip._
+
+  // Provide instances of Arbitrary Positive and Natural that allow the computation to complete in a reasonable
+  // amount of time and without Java heap space errors.
+  implicit val reasonablePositive: Arbitrary[Positive] =
+    Arbitrary(chooseRefinedNum[Refined, Long, RPositive](1L, 1000L))
+  implicit val reasonableNatural: Arbitrary[Natural] =
+    Arbitrary(chooseRefinedNum[Refined, Long, NonNegative](0L, 1000L))
+
+  def randBytes(size: Int): Task[Array[Byte]] = Task.delay {
+    val rand = new java.util.Random
+    Array.fill[Byte](size)(rand.nextInt.toByte)
+  }
+
+  /** A large, highly compressible stream of bytes: 1000 copies of the same
+    * 1000 random bytes, which can be read multiple times, but a different
+    * stream of bytes each time the outer task is run. */
+  def f4: Task[Process[Task, ByteVector]] =
+    randBytes(1000).map(bs => Process.emit(ByteVector.view(bs)).repeat.take(1000))
+
+  def unzip[A](f: java.io.InputStream => A)(p: Process[Task, ByteVector]): Task[List[(RelFile[Sandboxed], A)]] =
+    Task.delay {
+      val bytes = p.runLog.unsafePerformSync.toList.concatenate  // FIXME: this means we can't use this to test anything big
+      val is = new java.io.ByteArrayInputStream(bytes.toArray)
+      val zis = new java.util.zip.ZipInputStream(is)
+      Stream.continually(zis.getNextEntry).takeWhile(_ != null).map { entry =>
+        (posixCodec.parseRelFile(entry.getName).get relativeTo currentDir[Sandboxed]).get -> f(zis)
+      }.toList
+    }
+
+  // For testing, capture all the bytes from a process, parse them with a
+  // ZipInputStream, and capture just the size of the contents of each file.
+  def counts(p: Process[Task, ByteVector]): Task[List[(RelFile[Sandboxed], Natural)]] = {
+    def count(is: java.io.InputStream): Natural = {
+      def loop(n: Natural): Natural = if (is.read ≟ -1) n else loop(n |+| 1L)
+      loop(0L)
+    }
+    unzip(count)(p)
+  }
+
+  def bytes(p: Process[Task, ByteVector]): Task[List[(RelFile[Sandboxed], ByteVector)]] = {
+    def read(is: java.io.InputStream): ByteVector = {
+      def loop(acc: ByteVector): ByteVector = {
+        val buffer = new Array[Byte](16*1024)
+        val n = is.read(buffer)
+        if (n <= 0) acc else loop(acc ++ ByteVector.view(buffer).take(n.toLong))
+      }
+      loop(ByteVector.empty)
+    }
+    unzip(read)(p)
+  }
+
+  def bytesMapping(filesAndSize: Map[RelFile[Sandboxed], Positive])
+      : Task[Map[RelFile[Sandboxed], Process[Task, ByteVector]]] = {
+    def byteStream(size: Positive): Task[Process[Task, ByteVector]] =
+      randBytes(size.toInt).map(bytes => Process.emit(ByteVector.view(bytes)))
+    filesAndSize.toList.traverse { case (k, v) => byteStream(v).strengthL(k) }.map(_.toMap)
+  }
+
   "zipFiles" should {
-    import Zip._
-
-    // Provide instances of Arbitrary Positive and Natural that allow the computation to complete in a reasonable
-    // amount of time and without Java heap space errors.
-    implicit val reasonablePositive: Arbitrary[Positive] =
-      Arbitrary(chooseRefinedNum[Refined, Long, RPositive](1L, 1000L))
-    implicit val reasonableNatural: Arbitrary[Natural] =
-      Arbitrary(chooseRefinedNum[Refined, Long, NonNegative](0L, 1000L))
-
-    def rand = new java.util.Random
-    def randBlock = Array.fill[Byte](1000)(rand.nextInt.toByte)
-
-    def f4: Process[Task, ByteVector] = {
-      val block = ByteVector.view(randBlock)
-      Process.emitAll(Vector.fill(1000)(block))
-    }
-
-    def unzip[A](f: java.io.InputStream => A)(p: Process[Task, ByteVector]): Task[List[(RelFile[Sandboxed], A)]] =
-      Task.delay {
-        val bytes = p.runLog.unsafePerformSync.toList.concatenate  // FIXME: this means we can't use this to test anything big
-        val is = new java.io.ByteArrayInputStream(bytes.toArray)
-        val zis = new java.util.zip.ZipInputStream(is)
-        Stream.continually(zis.getNextEntry).takeWhile(_ != null).map { entry =>
-          (posixCodec.parseRelFile(entry.getName).get relativeTo currentDir[Sandboxed]).get -> f(zis)
-        }.toList
-      }
-
-    // For testing, capture all the bytes from a process, parse them with a
-    // ZipInputStream, and capture just the size of the contents of each file.
-    def counts(p: Process[Task, ByteVector]): Task[List[(RelFile[Sandboxed], Natural)]] = {
-      def count(is: java.io.InputStream): Natural = {
-        def loop(n: Natural): Natural = if (is.read ≟ -1) n else loop(n |+| 1L)
-        loop(0L)
-      }
-      unzip(count)(p)
-    }
-
-    def bytes(p: Process[Task, ByteVector]): Task[List[(RelFile[Sandboxed], ByteVector)]] = {
-      def read(is: java.io.InputStream): ByteVector = {
-        def loop(acc: ByteVector): ByteVector = {
-          val buffer = new Array[Byte](16*1024)
-          val n = is.read(buffer)
-          if (n <= 0) acc else loop(acc ++ ByteVector.view(buffer).take(n.toLong))
-        }
-        loop(ByteVector.empty)
-      }
-      unzip(read)(p)
-    }
-
     "zip files of constant bytes" >> prop { (filesAndSize: Map[RelFile[Sandboxed], Positive], byte: Byte) =>
-      def byteStream(size: Positive): Process[Task, ByteVector] =
-        Process.emit(ByteVector.view(Array.fill(size.toInt)(byte)))
-      val bytesMapping = filesAndSize.mapValues(byteStream)
-      val z = zipFiles(bytesMapping.toList)
+      val filesAndBytes = bytesMapping(filesAndSize).unsafePerformSync
+      val z = zipFiles(filesAndBytes.toList)
       counts(z).unsafePerformSync must_== filesAndSize.toList
     }.set(minTestsOk = 10) // This test is relatively slow
 
     "zip files of random bytes" >> prop { filesAndSize: Map[RelFile[Sandboxed], Positive] =>
-      def byteStream(size: Positive): Process[Task, ByteVector] =
-        Process.emit(ByteVector.view(Array.fill(size.toInt)(rand.nextInt.toByte)))
-      val bytesMapping = filesAndSize.mapValues(byteStream)
-      val z = zipFiles(bytesMapping.toList)
+      val filesAndBytes = bytesMapping(filesAndSize).unsafePerformSync
+      val z = zipFiles(filesAndBytes.toList)
       counts(z).unsafePerformSync must_== filesAndSize.toList
     }.set(minTestsOk = 10) // This test is relatively slow
 
@@ -113,7 +120,7 @@ class ZipSpecs extends quasar.Qspec {
       val MinExpectedSize = MaxExpectedSize / 2
 
       val paths = (0 until Files).toList.map(i => file[Sandboxed]("foo" + i))
-      val z = zipFiles(paths.map(_ -> f4))
+      val z = zipFiles(paths.map(_ -> f4.unsafePerformSync))
 
       // NB: can't use my naive `list` function on a large file
       z.map(_.size).sum.runLog.unsafePerformSync(0) must beBetween(MinExpectedSize, MaxExpectedSize)
@@ -127,17 +134,15 @@ class ZipSpecs extends quasar.Qspec {
       val MinExpectedSize = MaxExpectedSize / 2
 
       val paths = (0 until Files).toList.map(i => file[Sandboxed]("foo" + i))
-      val z = zipFiles(paths.map(_ -> f4))
+      val z = zipFiles(paths.map(_ -> f4.unsafePerformSync))
 
       // NB: can't use my naive `list` function on a large file
       z.map(_.size).sum.runLog.unsafePerformSync(0) must beBetween(MinExpectedSize, MaxExpectedSize)
     }
 
     "read twice without conflict" >> prop { filesAndSize: Map[RelFile[Sandboxed], Positive] =>
-      def byteStream(size: Positive): Process[Task, ByteVector] =
-        Process.emit(ByteVector.view(Array.fill(size.toInt)(rand.nextInt.toByte)))
-      val bytesMapping = filesAndSize.mapValues(byteStream)
-      val z = zipFiles(bytesMapping.toList)
+      val filesAndBytes = bytesMapping(filesAndSize).unsafePerformSync
+      val z = zipFiles(filesAndBytes.toList)
       bytes(z).unsafePerformSync must equal(bytes(z).unsafePerformSync)
     }.set(minTestsOk = 10) // This test is relatively slow
   }
