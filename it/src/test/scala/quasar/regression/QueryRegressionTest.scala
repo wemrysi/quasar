29649aa51745d338f41759983467b0579f203dd2
Pawel Szulc <paul.szulc@gmail.com>
2016-11-23 15:16:00 +0100

    Skip non-deterministic itests for spark

--- it/src/test/scala/quasar/regression/QueryRegressionTest.scala
+++ it/src/test/scala/quasar/regression/QueryRegressionTest.scala
@@ -137,7 +137,11 @@ abstract class QueryRegressionTest[S[_]](
     */
   def ensureTestData(testLoc: RFile, test: RegressionTest, run: Run): Task[Boolean] = {
     def ensureTestFile(loc: RFile): Task[Boolean] =
-      run(query.fileExists(dataFile(loc))).ifM(
+      run(query.fileExists(dataFile(loc))).map {
+        sth =>
+        println(s"############ ${test.name}, backends: ${test.backends}")
+        sth
+      }.ifM(
         false.point[Task],
         loadTestData(loc, run).as(true))
 
