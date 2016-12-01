a005b64818aa14a37874b64d79cb2abe2d58216a
Paul Phillips <paulp@improving.org>
2016-12-01 11:04:17 -0800

    Minimal compiling situation for StdLibEvaluatorStack.

--- fallback/src/main/scala/quasar/physical/fallback/fs/fallback.scala
+++ fallback/src/main/scala/quasar/physical/fallback/fs/fallback.scala
@@ -14,12 +14,33 @@
  * limitations under the License.
  */
 
-// package quasar.physical.fallback.fs
+package quasar.physical.fallback.fs
 
-// import scalaz._
-// // import Scalaz._
-// import com.precog.mimir._
+import quasar.Predef._
+import scalaz._
+import com.precog.mimir._
+import com.precog._, yggdrasil._, vfs._, table._
+import com.precog.common._, security._
 
-// object fall {
-//   object stack extends StdLibEvaluatorStack[Need]
-// }
+object fall {
+  object stack extends StdLibEvaluatorStack[Need] {
+    import trans.TransSpec1
+
+    type M[+A] = Need[A]
+    val M      = implicitly[Monad[Need]]
+
+    object vfs extends VFSMetadata[Need] {
+      def findDirectChildren(apiKey: APIKey, path: Path): EitherT[M, ResourceError, Set[PathMetadata]]                           = ???
+      def pathStructure(apiKey: APIKey, path: Path, property: CPath, version: Version): EitherT[M, ResourceError, PathStructure] = ???
+      def size(apiKey: APIKey, path: Path, version: Version): EitherT[M, ResourceError, Long]                                    = ???
+    }
+
+    trait TableCompanion extends ColumnarTableCompanion
+
+    object Table extends TableCompanion {
+      def apply(slices: StreamT[M, Slice], size: TableSize): Table                                                    = ???
+      def singleton(slice: Slice): Table                                                                              = ???
+      def align(sourceLeft: Table, alignOnL: TransSpec1, sourceRight: Table, alignOnR: TransSpec1): M[(Table, Table)] = ???
+    }
+  }
+}
