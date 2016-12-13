3e3ddeb6d9f710a2d1a5dd96f0af17e9dd30ad5d
Paul Phillips <paulp@improving.org>
2016-12-13 13:28:01 -0800

    to_s for table.

--- fallback/src/main/scala/quasar/physical/fallback/fs/fallback.scala
+++ fallback/src/main/scala/quasar/physical/fallback/fs/fallback.scala
@@ -17,8 +17,6 @@
 package quasar.physical.fallback.fs
 
 import quasar.Predef._
-import scalaz._
-import Scalaz._
 import blueeyes.yggConfig
 import blueeyes.json.JValue
 import com.precog.yggdrasil.vfs.StubVFSMetadata
@@ -26,6 +24,7 @@ import com.precog.bytecode.JType
 import com.precog.mimir._
 import com.precog._, yggdrasil._, vfs._, table._
 import com.precog.common._, security._
+import scalaz._, Scalaz._
 
 trait PrecogEvaluator[M[+_]] extends StdLibEvaluatorStack[M] with BlockStoreColumnarTableModule[M] {
   type ErrOr[A] = EitherT[M, ResourceError, A]
@@ -60,7 +59,11 @@ object PrecogEvaluator {
   }
 }
 
-object fall extends PrecogEvaluator[Need] {
-  val M           = implicitly[Monad[Need]]
+object fall extends PrecogEvaluator[Id.Id] {
+  val M           = implicitly[Monad[Id.Id]]
   val projections = Map[Path, Map[ColumnRef, Long]]()
+
+  implicit class TableOps(private val self: Table) {
+    def to_s: String = self.slices.toStream.copoint.toVector mkString "\n"
+  }
 }
