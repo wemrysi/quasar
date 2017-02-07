1bbb70513f3fd18ab96b8abd29a5eb12526143e8
Emrys Ingersoll <ingersoll@gmail.com>
2017-02-07 08:59:47 -0600

    Remove unnecessary import.

--- connector/src/test/scala/quasar/fs/ManageFileSpec.scala
+++ connector/src/test/scala/quasar/fs/ManageFileSpec.scala
@@ -20,7 +20,7 @@ import quasar.Predef._
 import quasar.Data
 import quasar.contrib.scalaz.eitherT._
 import quasar.contrib.scalaz.stateT._
-import quasar.fp._, ski._
+import quasar.fp.ski._
 
 import scala.Predef.$conforms
 
