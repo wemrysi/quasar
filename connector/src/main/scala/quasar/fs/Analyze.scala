1aaea5f7bd2c0ec17328e596880ae8ddc03460ed
Pawel Szulc <paul.szulc@gmail.com>
2017-04-14 18:04:32 +0200

    Add Analyze algebra

---
+++ connector/src/main/scala/quasar/fs/Analyze.scala
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2014–2017 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.fs
+
+import slamdata.Predef._
+import quasar.effect.LiftedOps
+import quasar.frontend.logicalplan.LogicalPlan
+
+import matryoshka.data.Fix
+import scalaz._
+
+sealed abstract class Analyze[A]
+
+object Analyze {
+
+  final case class QueryCost(lp: Fix[LogicalPlan]) extends Analyze[Int]
+
+  final class Unsafe[S[_]](implicit S: Analyze :<: S) extends LiftedOps[Analyze, S] {
+    def queryCost(lp: Fix[LogicalPlan]): Free[S, Int] = lift(QueryCost(lp))
+  }
+}
