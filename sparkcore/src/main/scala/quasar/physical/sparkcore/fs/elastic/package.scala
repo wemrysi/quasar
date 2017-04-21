951175da83d3688887977cccfe1717d10efb6573
Pawel Szulc <paul.szulc@gmail.com>
2017-04-21 13:33:12 +0200

    Init new (yet empty) spark-elastic connector

---
+++ sparkcore/src/main/scala/quasar/physical/sparkcore/fs/elastic/package.scala
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2014–2017 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.physical.sparkcore.fs
+
+import slamdata.Predef._
+import quasar.connector.EnvironmentError
+import quasar.contrib.pathy._
+import quasar.effect._
+import quasar.fp.free._
+import quasar.fs._
+import quasar.fs.mount._, FileSystemDef._
+import quasar.physical.sparkcore.fs.{genSc => coreGenSc}
+
+import java.net.URLDecoder
+
+import org.apache.spark._
+import org.http4s.{ParseFailure, Uri}
+import pathy.Path._
+import pathy.Path.posixCodec
+import scalaz.{Failure => _, _}, Scalaz._
+import scalaz.concurrent.Task
+
+
+package object elastic {
+
+  val FsType = FileSystemType("spark-elastic")
+
+  type Eff[A] = Coproduct[Task, PhysErr, A]
+
+  final case class SparkFSConf(sparkConf: SparkConf)
+
+  val parseUri: ConnectionUri => DefinitionError \/ (SparkConf, SparkFSConf) = (connUri: ConnectionUri) => {
+
+    def liftErr(msg: String): DefinitionError = NonEmptyList(msg).left[EnvironmentError]
+
+    def master(host: String, port: Int): State[SparkConf, Unit] =
+      State.modify(_.setMaster(s"spark://$host:$port"))
+
+    def appName: State[SparkConf, Unit] = State.modify(_.setAppName("quasar"))
+
+    val uriOrErr: DefinitionError \/ Uri = Uri.fromString(connUri.value).leftMap((pf: ParseFailure) => liftErr(pf.toString))
+
+    val sparkConfOrErr: DefinitionError \/ SparkConf = for {
+      uri <- uriOrErr
+      host <- uri.host.fold(NonEmptyList("host not provided").left[EnvironmentError].left[Uri.Host])(_.right[DefinitionError])
+      port <- uri.port.fold(NonEmptyList("port not provided").left[EnvironmentError].left[Int])(_.right[DefinitionError])
+    } yield {
+      (master(host.value, port) *> appName).exec(new SparkConf())
+    }
+
+    sparkConfOrErr.map(sparkConf => (sparkConf, SparkFSConf(sparkConf)))
+  }
+
+  private def sparkCoreJar: EitherT[Task, String, APath] = {
+    /* Points to quasar-web.jar or target/classes if run from sbt repl/run */
+    val fetchProjectRootPath = Task.delay {
+      val pathStr = URLDecoder.decode(this.getClass().getProtectionDomain.getCodeSource.getLocation.toURI.getPath, "UTF-8")
+      posixCodec.parsePath[Option[APath]](_ => None, Some(_).map(sandboxAbs), _ => None, Some(_).map(sandboxAbs))(pathStr)
+    }
+    val jar: Task[Option[APath]] =
+      fetchProjectRootPath.map(_.flatMap(s => parentDir(s).map(_ </> file("sparkcore.jar"))))
+    OptionT(jar).toRight("Could not fetch sparkcore.jar")
+  }
+
+  private def sparkFsDef[S[_]](implicit
+    S0: Task :<: S,
+    S1: PhysErr :<: S,
+    FailOps: Failure.Ops[PhysicalError, S]
+  ): SparkConf => Free[S, SparkFSDef[Eff, S]] = (sparkConf: SparkConf) => {
+
+    val genScWithJar: Free[S, String \/ SparkContext] = lift((for {
+      sc <- coreGenSc(sparkConf)
+      jar <- sparkCoreJar
+    } yield {
+      sc.addJar(posixCodec.printPath(jar))
+      sc
+    }).run).into[S]
+
+    val definition: SparkContext => Free[S, SparkFSDef[Eff, S]] = (sc: SparkContext) => lift(Task.delay {
+      val interpreter: Eff ~> S = injectNT[Task, S] :+: injectNT[PhysErr, S]
+      SparkFSDef(mapSNT[Eff, S](interpreter), lift(Task.delay(sc.stop())).into[S])
+    }).into[S]
+
+    genScWithJar >>= (_.fold(
+      msg => FailOps.fail[SparkFSDef[Eff, S]](UnhandledFSError(new RuntimeException(msg))),
+      definition(_)
+    ))
+  }
+
+  private val fsInterpret: SparkFSConf => (FileSystem ~> Free[Eff, ?]) = (sparkFsConf: SparkFSConf) => {
+
+    type FreeEff[A]  = Free[Eff, A]
+    interpretFileSystem(
+      Empty.queryFile[FreeEff],
+      Empty.readFile[FreeEff],
+      Empty.writeFile[FreeEff],
+      Empty.manageFile[FreeEff])
+  }
+
+  def definition[S[_]](implicit
+    S0: Task :<: S, S1: PhysErr :<: S
+  ) =
+    quasar.physical.sparkcore.fs.definition[Eff, S, SparkFSConf](FsType, parseUri, sparkFsDef, fsInterpret)
+
+}
