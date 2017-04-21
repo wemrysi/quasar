3c3ba239b4888ba20288df926de32aeb0fafa852
Pawel Szulc <paul.szulc@gmail.com>
2017-04-21 14:38:04 +0200

    Change signature of QueryFile.Input to return Free instead of Task

--- sparkcore/src/main/scala/quasar/physical/sparkcore/fs/local/queryfile.scala
+++ sparkcore/src/main/scala/quasar/physical/sparkcore/fs/local/queryfile.scala
@@ -24,6 +24,7 @@ import quasar.physical.sparkcore.fs.queryfile.Input
 import quasar.fs.FileSystemError._
 import quasar.contrib.pathy._
 import quasar.fp.ski._
+import quasar.fp.free._
 
 import java.io.{File, PrintWriter, FileOutputStream}
 import java.nio.file._
@@ -40,18 +41,24 @@ object queryfile {
       .map(raw => DataCodec.parse(raw)(DataCodec.Precise).fold(error => Data.NA, ι))
   }
 
-  def store(rdd: RDD[Data], out: AFile): Task[Unit] = Task.delay {
+  def store[S[_]](rdd: RDD[Data], out: AFile)(implicit
+    S: Task :<: S
+  ): Free[S, Unit] = lift(Task.delay {
     val ioFile = new File(posixCodec.printPath(out))
     val pw = new PrintWriter(new FileOutputStream(ioFile, false))
     rdd.flatMap(DataCodec.render(_)(DataCodec.Precise).toList).collect().foreach(v => pw.write(s"$v\n"))
     pw.close()
-  }
+  }).into[S]
 
-  def fileExists(f: AFile): Task[Boolean] = Task.delay {
+  def fileExists[S[_]](f: AFile)(implicit
+    S: Task :<: S
+  ): Free[S, Boolean] = lift(Task.delay {
     Files.exists(Paths.get(posixCodec.unsafePrintPath(f)))
-  }
+  }).into[S]
 
-  def listContents(d: ADir): EitherT[Task, FileSystemError, Set[PathSegment]] = EitherT(Task.delay {
+  def listContents[S[_]](d: ADir)(implicit
+    S: Task :<: S
+  ): EitherT[Free[S, ?], FileSystemError, Set[PathSegment]] = EitherT(lift(Task.delay {
     val directory = new File(posixCodec.unsafePrintPath(d))
     if(directory.exists()) {
       \/.fromTryCatchNonFatal{
@@ -65,9 +72,11 @@ object queryfile {
           pathErr(invalidPath(d, e.getMessage()))
       }
     } else pathErr(pathNotFound(d)).left[Set[PathSegment]]
-  })
+  }).into[S])
 
   def readChunkSize: Int = 5000
 
-  def input: Input = Input(fromFile _, store _, fileExists _, listContents _, readChunkSize _)
+  def input[S[_]](implicit
+    S: Task :<: S
+  ): Input[S] = Input[S](fromFile _, store[S] _, fileExists[S] _, listContents[S] _, readChunkSize _)
 }
