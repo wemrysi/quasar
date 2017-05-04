11622ce88e139be253255633eaa94c5ab9990b90
Pawel Szulc <paul.szulc@gmail.com>
2017-05-05 00:08:38 +0200

    Remove obsolete case

--- sparkcore/src/main/scala/quasar/physical/sparkcore/fs/CoreMap.scala
+++ sparkcore/src/main/scala/quasar/physical/sparkcore/fs/CoreMap.scala
@@ -372,7 +372,6 @@ object CoreMap extends Serializable {
 
   private def divide(d1: Data, d2: Data): Data = (d1, d2) match {
     case (Data.Dec(a), Data.Dec(b)) => Data.Dec(a(BigDecimal.defaultMathContext) / b)
-    case (Data.Dec(a), Data.Dec(b)) => Data.Dec(a / b)
     case (Data.Int(a), Data.Dec(b)) => Data.Dec(BigDecimal(a) / b)
     case (Data.Dec(a), Data.Int(b)) => Data.Dec(a(BigDecimal.defaultMathContext) / BigDecimal(b))
     case (Data.Int(a), Data.Int(b)) => Data.Dec(BigDecimal(a) / BigDecimal(b))
