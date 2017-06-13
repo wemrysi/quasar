36737728008e9df24e4d7240a8ac5aea30fa2b63
Pawel Szulc <paul.szulc@gmail.com>
2017-06-13 16:12:46 +0200

    Upgrade back to Spark 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -80,7 +80,7 @@ object Dependencies {
   )
 
   def mongodb = {
-    val nettyVersion = "4.0.29.Final" // This version is set to be the same as Spark
+    val nettyVersion = "4.0.42.Final" // This version is set to be the same as Spark
                                       // to avoid problems in web and it where their classpaths get merged
                                       // In any case, it should be binary compatible with version 4.0.26 that this
                                       // mongo release is expecting
@@ -101,7 +101,7 @@ object Dependencies {
   )
 
   def sparkcore(sparkProvided: Boolean) = Seq(
-    ("org.apache.spark" %% "spark-core" % "2.0.0" % (if(sparkProvided) "provided" else "compile"))
+    ("org.apache.spark" %% "spark-core" % "2.1.0" % (if(sparkProvided) "provided" else "compile"))
       .exclude("aopalliance", "aopalliance")                  // It seems crazy that we need to do this,
       .exclude("javax.inject", "javax.inject")                // but it looks like Spark had some dependency conflicts
       .exclude("commons-collections", "commons-collections")  // among its transitive dependencies which means
@@ -115,7 +115,7 @@ object Dependencies {
     "org.apache.parquet" % "parquet-hadoop" % "1.9.0",
     "org.http4s"         %% "http4s-core" % http4sVersion,
     "org.http4s"                 %% "http4s-blaze-client"       % http4sVersion,
-    "org.elasticsearch" % "elasticsearch-spark-20_2.11" % "5.3.0"
+    "org.elasticsearch" % "elasticsearch-spark-20_2.11" % "5.4.1"
   )
 
   def marklogic = Seq(
