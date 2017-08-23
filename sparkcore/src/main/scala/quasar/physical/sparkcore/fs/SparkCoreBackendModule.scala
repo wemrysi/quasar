cadaa3a97a41d0d1bbf81e4d88e0f12eb44fe209
Pawel Szulc <paul.szulc@gmail.com>
2017-08-23 10:19:56 +0200

    Implement ReadFileModule on SparkCoreBackendModule

--- sparkcore/src/main/scala/quasar/physical/sparkcore/fs/SparkCoreBackendModule.scala
+++ sparkcore/src/main/scala/quasar/physical/sparkcore/fs/SparkCoreBackendModule.scala
@@ -23,8 +23,8 @@ import quasar.contrib.pathy._
 import quasar.fp._, free._
 import quasar.fs._, FileSystemError._
 import quasar.fs.mount._, BackendDef.DefErrT
-import quasar.effect.{Read, Failure}
-import quasar.qscript._
+import quasar.effect._
+import quasar.qscript.{Read => _, _}
 
 import scala.Predef.implicitly
 
@@ -32,7 +32,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.SparkContext
 import matryoshka._
 import matryoshka.implicits._
-import scalaz.{Failure => _, _}
+import scalaz.{Failure => _, _}, Scalaz._
 import scalaz.concurrent.Task
 
 trait SparkCoreBackendModule extends BackendModule {
@@ -42,14 +42,23 @@ trait SparkCoreBackendModule extends BackendModule {
   def toLowerLevel[S[_]](implicit S0: Task :<: S, S1: PhysErr :<: S): M ~> Free[S, ?]
   def generateSC: Config => DefErrT[M, SparkContext]
 
-  def ReadSparkContext: Inject[Read[SparkContext, ?], Eff]
+  def ReadSparkContextInj: Inject[Read[SparkContext, ?], Eff]
+  def KeyValueStoreInj: Inject[KeyValueStore[ReadFile.ReadHandle, SparkCursor, ?], Eff]
+  def MonotonicSeqInj: Inject[MonotonicSeq, Eff]
+  def TaskInj: Inject[Task, Eff]
+  def SparkConnectorDetailsInj: Inject[SparkConnectorDetails, Eff]
 
   // common for all spark based connecotrs
+
   type M[A] = Free[Eff, A]
   type QS[T[_[_]]] = QScriptCore[T, ?] :\: EquiJoin[T, ?] :/: Const[ShiftedRead[AFile], ?]
   type Repr = RDD[Data]
 
-  private final implicit def _ReadSparkContext: Inject[Read[SparkContext, ?], Eff] = ReadSparkContext
+  private final implicit def _ReadSparkContextInj: Inject[Read[SparkContext, ?], Eff] = ReadSparkContextInj
+  private final implicit def _KeyValueStoreInj: Inject[KeyValueStore[ReadFile.ReadHandle, SparkCursor, ?], Eff] = KeyValueStoreInj
+  private final implicit def _MonotonicSeqInj: Inject[MonotonicSeq, Eff] = MonotonicSeqInj
+  private final implicit def _TaskInj: Inject[Task, Eff] = TaskInj
+  private final implicit def _SparkConnectorDetailsInj: Inject[SparkConnectorDetails, Eff] = SparkConnectorDetailsInj
 
   def FunctorQSM[T[_[_]]] = Functor[QSM[T, ?]]
   def DelayRenderTreeQSM[T[_[_]]: BirecursiveT: EqualT: ShowT: RenderTreeT] =
@@ -80,6 +89,19 @@ trait SparkCoreBackendModule extends BackendModule {
       cp.cataM(total.plan(rddFrom, first)).eval(sc).run.map(_.leftMap(pe => qscriptPlanningFailed(pe)))
     }).liftB)
 
+  object SparkReadFileModule extends ReadFileModule {
+    import ReadFile._
+    import quasar.fp.numeric.{Natural, Positive}
+
+    def open(file: AFile, offset: Natural, limit: Option[Positive]): Backend[ReadHandle] =
+      includeError(readfile.open[Eff](file, offset, limit).liftB)
+    def read(h: ReadHandle): Backend[Vector[Data]] =
+      includeError(readfile.read[Eff](h).liftB)
+    def close(h: ReadHandle): Configured[Unit] =
+      readfile.close[Eff](h).liftM[ConfiguredT]
+  }
+  def ReadFileModule = SparkReadFileModule
+  
   // util functions because 4 levels of monad transformes is like o_O
   // this will disapper
   def includeError[A](b: Backend[FileSystemError \/ A]): Backend[A]
