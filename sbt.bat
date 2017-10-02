f053c17c4b9e3d0833f7764d69375ce4dbf384a4
Daniel Spiewak <daniel@slamdata.com>
2017-10-02 10:44:32 -0600

    Added roll-your-own sbt-extras script for windows

---
+++ sbt.bat
@@ -0,0 +1,29 @@
+@REM https://github.com/xerial/sbt-pack/blob/master/src/main/templates/launch-bat.mustache
+@REM would be worth getting more inspiration from
+
+@echo off
+
+SET ERROR_CODE=0
+
+SET LAUNCHER_PATH=%localappdata%/coursier
+
+IF NOT EXIST "%LAUNCHER_PATH%" (
+  bitsadmin /transfer "DownloadCoursierLauncher" https://github.com/coursier/coursier/raw/master/coursier "%LAUNCHER_PATH%"
+)
+
+SET CMD_LINE_ARGS=%*
+
+java -Xms512m -Xmx4g -Xss2m -Dfile.encoding=UTF8 -jar "%LAUNCHER_PATH%" launch org.scala-sbt:sbt-launch:1.0.2 %CMD_LINE_ARGS%
+
+IF ERRORLEVEL 1 GOTO error
+GOTO end
+
+:error
+SET ERROR_CODE=1
+
+:end
+SET LAUNCHER_PATH=
+SET CMD_LINE_ARGS=
+
+EXIT /B %ERROR_CODE%
+
