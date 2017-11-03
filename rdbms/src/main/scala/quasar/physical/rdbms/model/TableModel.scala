ab44dc3e7cef4317a5ac25595479d7f1b5cb2f23
Krzysiek Ciesielski <kciesielski@users.noreply.github.com>
2017-11-03 13:08:21 +0100

    Table model deviser

---
+++ rdbms/src/main/scala/quasar/physical/rdbms/model/TableModel.scala
@@ -0,0 +1,132 @@
+/*
+ * Copyright 2014–2017 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.physical.rdbms.model
+
+import slamdata.Predef._
+import quasar.Data
+import quasar.fs.FileSystemError
+import quasar.fs.FileSystemError._
+
+import scalaz._
+import Scalaz._
+
+sealed trait ColumnType
+case object JsonCol extends ColumnType
+case object StringCol extends ColumnType
+case object IntCol extends ColumnType
+case object NullCol extends ColumnType
+
+final case class ColumnDesc(name: String, tpe: ColumnType)
+
+sealed trait TableModel
+
+case object JsonTable extends TableModel
+
+final case class ColumnarTable(columns: ISet[ColumnDesc])
+  extends TableModel
+
+object TableModel {
+
+  implicit val columnDescOrder: Order[ColumnDesc] = Order.orderBy(_.name)
+
+  implicit val columnTypeEq: Equal[ColumnType] = Equal.equalA
+
+  /**
+    * When updating current table model with new model, the resulting model
+    * can be either extended by new columns, or just a JsonTable. The latter
+    * means that current and new models cannot produce a new definition which
+    * reconciles data types for both (data is heterogenous).
+    */
+  implicit val tableModelMonoid: Monoid[TableModel] = new Monoid[TableModel] {
+    override def zero: TableModel = ColumnarTable(ISet.empty)
+    def empty: TableModel = zero
+
+    override def append(s1: TableModel, s2: => TableModel): TableModel = {
+
+      def updateCols(cols: ISet[ColumnDesc],
+                     newCols: ISet[ColumnDesc]): TableModel = {
+        cols.toList
+          .traverse { c =>
+            newCols
+              .lookupIndex(c)
+              .flatMap(newCols.elemAt)
+              .map { newC =>
+                if (c.tpe === NullCol)
+                  newC.some
+                else if (c.tpe === newC.tpe || newC.tpe === NullCol)
+                  c.some
+                else
+                  none
+              }
+              .getOrElse(c.some)
+          }
+          .map(cs =>
+            ColumnarTable(ISet.fromList(cs) ⊹ newCols.difference(cols)): TableModel)
+          .getOrElse(JsonTable)
+      }
+
+      s1 match {
+        case JsonTable                           => JsonTable
+        case ColumnarTable(cols) if cols.isEmpty => s2
+        case ColumnarTable(cols) =>
+          s2 match {
+            case JsonTable                                 => JsonTable
+            case ColumnarTable(newCols) if newCols.isEmpty => s1
+            case ColumnarTable(newCols)                    =>
+              updateCols(cols, newCols)
+          }
+      }
+    }
+  }
+
+
+  def simpleColumnType(data: Data): ColumnType = {
+    data match {
+      case Data.Null   => NullCol
+      case Data.Str(_) => StringCol
+      case Data.Int(_) => IntCol
+      case _           => NullCol // TODO support all types
+    }
+  }
+
+  def columnType(data: Data): ColumnType = {
+    data match {
+      case Data.Obj(_) => JsonCol
+      case Data.Arr(_) => JsonCol
+      case _           => simpleColumnType(data)
+    }
+  }
+
+  def rowModel(row: Data): TableModel = {
+    row match {
+      case Data.Obj(fields) =>
+        ColumnarTable(ISet.fromList(fields.map {
+          case (label, value) => ColumnDesc(label, columnType(value))
+        }.toList))
+      case _ => JsonTable
+    }
+  }
+
+  def fromData(ds: Vector[Data]): FileSystemError \/ TableModel = {
+    ds match {
+      case _ :+ _ =>
+        ds.foldMap(rowModel).right
+      case _ =>
+        unsupportedOperation("Cannot map empty data row to a RDBMS table model.").left
+    }
+  }
+}
---
+++ rdbms/src/test/scala/quasar/physical/rdbms/model/TableModelTest.scala
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2014–2017 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.physical.rdbms.model
+
+import quasar.{Data, DataCodec, Qspec}
+import slamdata.Predef._
+import TableModel._
+
+import org.scalacheck.Gen
+import org.scalacheck.Prop._
+import scalaz._
+import Scalaz._
+
+class TableModelTest extends Qspec {
+
+  implicit val codec: DataCodec = DataCodec.Precise
+
+  def data(str: String): Data =
+    DataCodec.parse(str).valueOr(err => scala.sys.error(err.shows))
+  
+  def permutations[T](v: Vector[T]): Gen[Vector[T]] = {
+    val perms = v.permutations
+    Gen.oneOf(perms.toList)
+  }
+
+  "Table Model" should {
+
+    "fail for empty vector" in {
+      TableModel.fromData(Vector.empty) should beLeftDisjunction
+    }
+
+    "devise columnar schema for a single row" in {
+      val row = data("""{"name":"John","surname":"Smith","birthYear":1980}""")
+      TableModel.fromData(Vector(row)) should beRightDisjunction(
+        ColumnarTable(
+          ISet.fromList(List(ColumnDesc("name", StringCol),
+            ColumnDesc("birthYear", IntCol),
+            ColumnDesc("surname", StringCol)))))
+    }
+
+    "devise columnar schema for homogenous data" in {
+      val row = data("""{"age": 25,"id":"748abf","keys": [4, 5, 6, 7]}""")
+      TableModel.fromData(Vector(row, row, row, row, row, row)) should beRightDisjunction(
+        ColumnarTable(
+          ISet.fromList(List(ColumnDesc("age", IntCol),
+            ColumnDesc("keys", JsonCol),
+            ColumnDesc("id", StringCol)))))
+    }
+
+    "devise columnar schema when extra columns appear" in {
+        val row = data("""{"age": 25,"id":"748abf","keys": [4, 5, 6, 7]}""")
+        val updatedRow1 =
+          data("""{"age": 35,"id":"a748abf","keys": [], "extra-col" : "v1"}""")
+        val updatedRow2 = data(
+          """{"age": 45, "another-extra-col": "v2", "id":"11","keys": [222]}""")
+
+        forAll(permutations(Vector(row,
+          row,
+          updatedRow1,
+          row,
+          updatedRow2,
+          row,
+          row))) { rows =>
+
+        TableModel.fromData(rows) should beRightDisjunction(
+          ColumnarTable(ISet.fromList(List(
+            ColumnDesc("age", IntCol),
+            ColumnDesc("keys", JsonCol),
+            ColumnDesc("id", StringCol),
+            ColumnDesc("extra-col", StringCol),
+            ColumnDesc("another-extra-col", StringCol)
+          ))))
+      }
+    }
+
+    "devise columnar schema when some columns get nulled" in {
+        val row = data("""{"age": 25,"id":"748abf","keys": [4, 5, 6, 7]}""")
+        val updatedRow1 = data("""{"id":"a748abf","keys": []}""")
+
+        forAll(permutations(
+          Vector(row, row, updatedRow1, row, row))) { rows =>
+
+        TableModel.fromData(rows) should beRightDisjunction(
+          ColumnarTable(
+            ISet.fromList(List(ColumnDesc("age", IntCol),
+              ColumnDesc("keys", JsonCol),
+              ColumnDesc("id", StringCol)))))
+      }
+    }
+
+    "devise columnar schema when some columns get nulled and some added" in {
+        val row = data("""{"age": 25,"id":"748abf","keys": [4, 5, 6, 7]}""")
+        val updatedRow1 = data("""{"id":"a748abf","keys": []}""")
+        val updatedRow2 = data("""{"extra": {"nested1": "val"}, "id":"a748abf","keys": []}""")
+
+        forAll(permutations(Vector(row,
+          row,
+          updatedRow1,
+          row,
+          row,
+          updatedRow2))) { rows =>
+
+        TableModel.fromData(rows) should beRightDisjunction(
+          ColumnarTable(
+            ISet.fromList(
+              List(ColumnDesc("age", IntCol),
+                ColumnDesc("keys", JsonCol),
+                ColumnDesc("id", StringCol),
+                ColumnDesc("extra", JsonCol)))))
+      }
+    }
+
+    "devise columnar schema when all columns are nulled and some row has only new columns" in {
+        val row = data("""{"age": 25,"id":"748abf","keys": [4, 5, 6, 7]}""")
+        val updatedRow1 = data("""{}""")
+        val updatedRow2 = data("""{"extra": 35, "extra2": "str value"}""")
+
+      forAll(permutations(Vector(row,
+        row,
+        updatedRow1,
+        row,
+        row,
+        updatedRow2))) { rows =>
+
+        TableModel.fromData(rows) should beRightDisjunction(
+          ColumnarTable(
+            ISet.fromList(
+              List(ColumnDesc("age", IntCol),
+                ColumnDesc("keys", JsonCol),
+                ColumnDesc("id", StringCol),
+                ColumnDesc("extra", IntCol),
+                ColumnDesc("extra2", StringCol)))))
+      }
+    }
+
+    "devise json schema on encountering incompatible data types" in {
+      val row1 = data("""{"age": 25,"id":"748abf","keys": [4, 5, 6, 7]}""")
+      val row2 = data("""{"age": "25","id":"648abf","keys": [4, 5, 6, 7]}""")
+
+      forAll(permutations(Vector(row1, row2, row2, row2, row1))) { rows =>
+
+        TableModel.fromData(rows) should beRightDisjunction(JsonTable)
+      }
+    }
+
+    "treat nulls as 'always compatible' column types" in {
+      val row1 = data("""{"age": 25,"id":"748abf", "name": "Bob"}""")
+      val row2 = data("""{"age": 35,"id":"648abf", "name": null}""")
+
+      forAll(permutations(Vector(row1, row2, row2, row2, row1))) { rows =>
+
+        TableModel.fromData(rows) should beRightDisjunction(
+          ColumnarTable(
+            ISet.fromList(
+              List(ColumnDesc("age", IntCol),
+                ColumnDesc("id", StringCol),
+                ColumnDesc("name", StringCol)))))
+      }
+    }
+  }
+}
