7ba8f7d5bd3e941bc90712432b52fd801b9e6614
Daniel Spiewak <daniel@slamdata.com>
2017-11-06 12:41:07 +0000

    Added missing QSUGraph.Read extractor

--- connector/src/main/scala/quasar/qscript/qsu/QSUGraph.scala
+++ connector/src/main/scala/quasar/qscript/qsu/QSUGraph.scala
@@ -128,6 +128,13 @@ object QSUGraph extends QSUGraphInstances {
       }
     }
 
+    object Read {
+      def unapply[T[_[_]]](g: QSUGraph[T]) = g.unfold match {
+        case g: QSU.Read[T, QSUGraph[T]] => QSU.Read.unapply(g)
+        case _ => None
+      }
+    }
+
     object Map {
       def unapply[T[_[_]]](g: QSUGraph[T]) = g.unfold match {
         case g: QSU.Map[T, QSUGraph[T]] => QSU.Map.unapply(g)
