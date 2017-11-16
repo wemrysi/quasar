71ffc22bbddfa0f8958a399b87fd2b3d812ccb71
rintcius <rintcius@gmail.com>
2017-11-16 17:46:59 +0100

    Move planner log tests into separate spec

--- mongodb/src/test/scala/quasar/physical/mongodb/PlannerSpec.scala
+++ mongodb/src/test/scala/quasar/physical/mongodb/PlannerSpec.scala
@@ -2589,45 +2589,4 @@ class PlannerSpec extends
               ExcludeId)))
     }
   }
-
-  "planner log" should {
-    "include all phases when successful" in {
-      planLog(sqlE"select city from zips").map(_.name) must_===
-        Vector(
-          "SQL AST", "Variables Substituted", "Absolutized", "Normalized Projections",
-          "Sort Keys Projected", "Annotated Tree",
-          "Logical Plan", "Optimized", "Typechecked", "Rewritten Joins",
-          "QScript", "QScript (ShiftRead)", "QScript (Optimized)",
-          "QScript Mongo", "QScript Mongo (Subset Before Map)", "QScript Mongo (Prefer Projection)",
-          "Workflow Builder", "Workflow (raw)", "Workflow (crystallized)")
-    }
-
-    "log mapBeforeSort when it is applied" in {
-      planLog(sqlE"select length(city) from zips order by city").map(_.name) must_===
-        Vector(
-          "SQL AST", "Variables Substituted", "Absolutized", "Normalized Projections",
-          "Sort Keys Projected", "Annotated Tree",
-          "Logical Plan", "Optimized", "Typechecked", "Rewritten Joins",
-          "QScript", "QScript (ShiftRead)", "QScript (Optimized)",
-          "QScript Mongo", "QScript Mongo (Subset Before Map)",
-          "QScript Mongo (Prefer Projection)", "QScript Mongo (Map Before Sort)",
-          "Workflow Builder", "Workflow (raw)", "Workflow (crystallized)")
-    }
-
-    "include correct phases with type error" in {
-      planLog(sqlE"select 'a' + 0 from zips").map(_.name) must_===
-        Vector(
-          "SQL AST", "Variables Substituted", "Absolutized", "Annotated Tree", "Logical Plan", "Optimized")
-    }.pendingUntilFixed("SD-1249")
-
-    "include correct phases with planner error" in {
-      planLog(sqlE"""select interval(bar) from zips""").map(_.name) must_===
-        Vector(
-          "SQL AST", "Variables Substituted", "Absolutized", "Normalized Projections",
-          "Sort Keys Projected", "Annotated Tree",
-          "Logical Plan", "Optimized", "Typechecked", "Rewritten Joins",
-          "QScript", "QScript (ShiftRead)", "QScript (Optimized)",
-          "QScript Mongo", "QScript Mongo (Subset Before Map)", "QScript Mongo (Prefer Projection)")
-    }
-  }
 }
