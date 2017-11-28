9db4ad677ae42813cae63f71f318f84045f5fcbb
Alissa Pajer <alissa.pajer@gmail.com>
2017-11-28 16:29:25 -0700

    Reenable a spec for ShiftRead.

--- connector/src/test/scala/quasar/qscript/ShiftReadSpec.scala
+++ connector/src/test/scala/quasar/qscript/ShiftReadSpec.scala
@@ -16,6 +16,8 @@
 
 package quasar.qscript
 
+import slamdata.Predef.{List, Nil}
+
 import quasar.TreeMatchers
 import quasar.contrib.pathy.AFile
 import quasar.fp._
@@ -33,12 +35,12 @@ class ShiftReadSpec extends quasar.Qspec with QScriptHelpers with TreeMatchers {
     "eliminate Read nodes from a simple query" in {
       val sampleFile = rootDir </> file("bar")
 
-      val qScript =
+      val qScript: Fix[QS] =
         chainQS(
           qsdsl.fix.Read[AFile](sampleFile),
           qsdsl.fix.LeftShift(_, qsdsl.func.Hole, ExcludeId, qsdsl.func.RightSide))
 
-      val newQScript =
+      val newQScript: Fix[QST] =
         qScript.codyna(
           rewrite.normalizeTJ[QST] >>> (_.embed),
           ((_: Fix[QS]).project) >>> (ShiftRead[Fix, QS, QST].shiftRead(idPrism.reverseGet)(_)))
@@ -50,22 +52,29 @@ class ShiftReadSpec extends quasar.Qspec with QScriptHelpers with TreeMatchers {
             qstdsl.func.ProjectIndexI(qstdsl.func.Hole, 1)))
     }
 
-    //TODO re-enable
-    //"shift a simple aggregated read" in {
-    //  import qstdsl._
-    //  convert(lc.some,
-    //    structural.MakeMap(
-    //      lpf.constant(Data.Str("0")),
-    //      agg.Count(lpRead("/foo/bar")).embed).embed).map(
-    //    _.codyna(
-    //      rewrite.normalizeTJ[QST] >>> (_.embed),
-    //      ((_: Fix[QS]).project) >>> (ShiftRead[Fix, QS, QST].shiftRead(idPrism.reverseGet)(_)))) must
-    //    beTreeEqual(
-    //      fix.Reduce(
-    //        fix.ShiftedRead[AFile](rootDir </> dir("foo") </> file("bar"), IncludeId),
-    //        Nil,
-    //        List(ReduceFuncs.Count(func.ProjectIndexI(func.Hole, 1))),
-    //        func.MakeMapS("0", func.ReduceIndex(0.right))).some)
-    //}
+    "shift a simple aggregated read" in {
+      val qScript: Fix[QS] = qsdsl.fix.Reduce(
+        qsdsl.fix.LeftShift(
+          qsdsl.fix.Read[AFile](rootDir </> dir("foo") </> file("bar")),
+          qsdsl.func.Hole,
+          ExcludeId,
+          qsdsl.func.RightSide),
+        Nil,
+        List(ReduceFuncs.Count(qsdsl.func.Hole)),
+        qsdsl.func.MakeMapS("0", qsdsl.func.ReduceIndex(0.right)))
+
+      val newQScript: Fix[QST] =
+        qScript.codyna(
+          rewrite.normalizeTJ[QST] >>> (_.embed),
+          ((_: Fix[QS]).project) >>> (ShiftRead[Fix, QS, QST].shiftRead(idPrism.reverseGet)(_)))
+
+      newQScript must
+        beTreeEqual(
+          qstdsl.fix.Reduce(
+            qstdsl.fix.ShiftedRead[AFile](rootDir </> dir("foo") </> file("bar"), IncludeId),
+            Nil,
+            List(ReduceFuncs.Count(qstdsl.func.ProjectIndexI(qstdsl.func.Hole, 1))),
+            qstdsl.func.MakeMapS("0", qstdsl.func.ReduceIndex(0.right))))
+    }
   }
 }
