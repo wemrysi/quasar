bee3beb01ca93017f8a7b97703358b3021d626b9
Daniel Spiewak <daniel@slamdata.com>
2017-12-11 11:41:48 -0700

    Implemented dimensional 1-2 coalescence

--- connector/src/test/scala/quasar/qscript/qsu/MinimizeAutoJoinsSpec.scala
+++ connector/src/test/scala/quasar/qscript/qsu/MinimizeAutoJoinsSpec.scala
@@ -22,14 +22,17 @@ import quasar.ejson.implicits._
 import quasar.fp._
 import quasar.qscript.{
   construction,
+  ExcludeId,
   Hole,
   HoleF,
   LeftSide,
+  LeftSideF,
   MapFuncsCore,
   ReduceFuncs,
   ReduceIndex,
   ReduceIndexF,
   RightSide,
+  RightSideF,
   SrcHole
 }
 import slamdata.Predef._
@@ -425,6 +428,30 @@ object MinimizeAutoJoinsSpec extends Qspec with TreeMatchers with QSUTTypes[Fix]
       }
 
       ok
+    }
+
+    "coalesce an autojoin on a single leftshift on a shared source" in {
+      val qgraph = QSUGraph.fromTree[Fix](
+        qsu.autojoin2((
+          qsu.leftShift(
+            qsu.read(afile),
+            HoleF[Fix],
+            ExcludeId,
+            RightSideF[Fix]),
+          qsu.read(afile),
+          _(MapFuncsCore.Add(_, _)))))
+
+      runOn(qgraph) must beLike {
+        case LeftShift(
+          Read(`afile`),
+          struct,
+          ExcludeId,
+          repair) =>
+
+          struct must_=== HoleF[Fix]
+
+          repair must beTreeEqual(func.Add(LeftSideF, RightSideF))
+      }
     }
   }
 
