17a6a2a11e936ecf69b6c48318553ab86856951e
rintcius <rintcius@gmail.com>
2018-02-16 12:26:36 +0100

    reinstate caching tests

--- web/src/test/scala/quasar/api/services/query/ExecuteServiceSpec.scala
+++ web/src/test/scala/quasar/api/services/query/ExecuteServiceSpec.scala
@@ -25,15 +25,20 @@ import quasar.api.services.VCacheFixture
 import quasar.api.services.Fixture._
 import quasar.common.{Map => _, _}
 import quasar.contrib.pathy._, PathArbitrary._
+import quasar.DateArbitrary._
 import quasar.effect.ScopeExecution
 import quasar.fp._
 import quasar.fp.free.{foldMapNT, injectNT, liftFT}
 import quasar.fp.ski._
 import quasar.fp.numeric._
 import quasar.fs._, InMemory._, mount._
+import quasar.fs.mount.cache.ViewCache
+import quasar.fs.mount.MountConfig.viewConfig0
 import quasar.frontend.logicalplan.{LogicalPlan, LogicalPlanR}
 import quasar.main.CoreEffIO
 import quasar.sql.{Positive => _, _}
+
+import java.time.{Instant, Duration}
 
 import argonaut.{Json => AJson, _}, Argonaut._
 import eu.timepit.refined.api.Refined
@@ -41,8 +46,10 @@ import eu.timepit.refined.auto._
 import eu.timepit.refined.numeric.{NonNegative, Positive => RPositive}
 import eu.timepit.refined.scalacheck.numeric._
 import matryoshka.data.Fix
+import org.http4s
 import org.http4s._
 import org.http4s.argonaut._
+import org.http4s.headers._
 import org.specs2.matcher.MatchResult
 import pathy.Path._
 import pathy.scalacheck.PathyArbitrary._
@@ -52,6 +59,7 @@ import rapture.json._, jsonBackends.json4s._, patternMatching.exactObjects._
 import scalaz._, Scalaz._
 import scalaz.concurrent.Task
 import scalaz.stream.Process
+import shapeless.tag.@@
 import quasar.api.PathUtils._
 
 class ExecuteServiceSpec extends quasar.Qspec with FileSystemFixture with Http4s {
@@ -146,6 +154,68 @@ class ExecuteServiceSpec extends quasar.Qspec with FileSystemFixture with Http4s
           state = emptyMem,
           status = Status.Ok,
           response = (a: String) => a must_=== "2\r\n")
+      }
+    }
+    "response with view cache headers" >> {
+      "fresh" >> prop {
+          (now: Instant, lastUpdate: Instant, maxAgeSecs: Int @@ RPositive) => {
+          val maxAge = Duration.ofSeconds(maxAgeSecs.toLong)
+          lastUpdate.isBefore(Instant.MAX.minus(maxAge)) && now.isBefore(lastUpdate.plus(maxAge))
+        } ==> {
+          val f = rootDir </> file("f")
+          val g = rootDir </> file("g")
+          val expr = sqlB"""select { "α": 7 }"""
+          val viewCache = ViewCache(
+            MountConfig.ViewConfig(expr, Variables.empty), lastUpdate.some, None, 0, None, None,
+            maxAgeSecs.toLong, Instant.ofEpochSecond(0), ViewCache.Status.Pending, None, g, None)
+          val mounts = Map[APath, MountConfig](f -> viewConfig0(expr))
+
+          val (resp, vc) = evalViewTest(now, mounts, InMemState.empty) { (it, ir) =>
+            (for {
+              _ <- vcache.put(f, viewCache)
+              a <- execute.service[ViewEff, Nothing](executionIdRef).apply(
+                     Request(uri = (pathUri(f) / "").copy(query =
+                       http4s.Query.fromPairs(
+                         "q" -> s"select * from `../${fileName(f).value}`"))))
+              c <- vcache.get(f).run
+            } yield (a.toHttpResponse(ir), c)).foldMap(it)
+          }.unsafePerformSync
+
+          resp.status must_= Status.Ok
+          resp.headers.get(Expires.name) ∘ (_.value) must_= None
+          vc ∘ (_.cacheReads) must_= (viewCache.cacheReads ⊹ 1).some
+        }
+      }
+
+      "stale" >> prop {
+          (now: Instant, lastUpdate: Instant, maxAgeSecs: Int @@ RPositive) => {
+          val maxAge = Duration.ofSeconds(maxAgeSecs.toLong)
+            lastUpdate.isBefore(Instant.MAX.minus(maxAge)) && now.isAfter(lastUpdate.plus(maxAge))
+        } ==> {
+          val f = rootDir </> file("f")
+          val g = rootDir </> file("g")
+          val expr = sqlB"""select { "α": 7 }"""
+          val viewCache = ViewCache(
+            MountConfig.ViewConfig(expr, Variables.empty), lastUpdate.some, None, 0, None, None,
+            maxAgeSecs.toLong, Instant.ofEpochSecond(0), ViewCache.Status.Pending, None, g, None)
+          val mounts = Map[APath, MountConfig](f -> viewConfig0(expr))
+
+          val (resp, vc) = evalViewTest(now, mounts, InMemState.empty) { (it, ir) =>
+            (for {
+              _ <- vcache.put(f, viewCache)
+              a <- execute.service[ViewEff, Nothing](executionIdRef).apply(
+                Request(uri = (pathUri(f) / "").copy(query =
+                  http4s.Query.fromPairs(
+                    "q" -> s"select * from `../${fileName(f).value}`"))))
+              c <- vcache.get(f).run
+            } yield (a.toHttpResponse(ir), c)).foldMap(it)
+          }.unsafePerformSync
+
+          resp.status must_= Status.Ok
+          resp.headers.get(Warning) ∘ (_.value) must_= None
+          resp.headers.get(Expires) ∘ (_.value) must_= None
+          vc ∘ (_.cacheReads) must_= (viewCache.cacheReads ⊹ 1).some
+        }
       }
     }
     "execute a query with offset and limit and a variable" >> {
