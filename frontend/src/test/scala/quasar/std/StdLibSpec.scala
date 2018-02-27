c68f7caf0d1262c220f4621bf804a3cf4d309673
Alissa Pajer <alissa.pajer@gmail.com>
2018-02-27 09:43:43 -0700

    Add any value with self tests for Eq and Neq.

--- frontend/src/test/scala/quasar/std/StdLibSpec.scala
+++ frontend/src/test/scala/quasar/std/StdLibSpec.scala
@@ -2321,6 +2321,8 @@ abstract class StdLibSpec extends Qspec {
     "RelationsLib" >> {
       import RelationsLib._
 
+      // TODO: comparisons for date/time/interval types
+
       "Eq" >> {
         "any two Ints" >> prop { (x: BigInt, y: BigInt) =>
           binary(Eq(_, _).embed, Data.Int(x), Data.Int(y), Data.Bool(x == y))
@@ -2344,6 +2346,10 @@ abstract class StdLibSpec extends Qspec {
             !((Type.Numeric contains x.dataType) &&
               (Type.Numeric contains y.dataType))) ==>
             binary(Eq(_, _).embed, x, y, Data.Bool(false))
+        }
+
+        "any value with self" >> prop { (x: Data) =>
+          binary(Eq(_, _).embed, x, x, Data.Bool(true))
         }
       }
 
@@ -2372,7 +2378,9 @@ abstract class StdLibSpec extends Qspec {
             binary(Neq(_, _).embed, x, y, Data.Bool(true))
         }
 
-        // TODO: the rest of the types
+        "any value with self" >> prop { (x: Data) =>
+          binary(Neq(_, _).embed, x, x, Data.Bool(false))
+        }
       }
 
       "Lt" >> {
