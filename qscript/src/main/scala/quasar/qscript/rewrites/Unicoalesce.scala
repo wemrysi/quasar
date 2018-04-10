6e7e06adec13118007ae21435c3cee1b4cbbbde7
Łukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2018-04-10 09:18:26 +0200

    Fix Unicoalesce

--- qscript/src/main/scala/quasar/qscript/rewrites/Unicoalesce.scala
+++ qscript/src/main/scala/quasar/qscript/rewrites/Unicoalesce.scala
@@ -25,8 +25,8 @@ import quasar.qscript._
 import matryoshka._
 import scalaz._
 
-sealed trait Unicoalesce[T[_[_]], C <: CoM] {
-  def apply(C: Coalesce.Aux[T, C#M, C#M])(implicit F: Functor[C#M]): C#M[T[C#M]] => Option[C#M[T[C#M]]]
+sealed trait Unicoalesce[T[_[_]], C[_] <: ACopK] {
+  def apply(C: Coalesce.Aux[T, C, C])(implicit F: Functor[C]): C[T[C]] => Option[C[T[C]]]
 }
 
 object Unicoalesce {
@@ -36,30 +36,30 @@ object Unicoalesce {
    * dependencies in a single implicit, which can then be passed
    * along as a value (e.g. in BackendModule).
    */
-  trait Capture[T[_[_]], C <: CoM] {
-    val C: Coalesce.Aux[T, C#M, C#M]
-    val F: Functor[C#M]
+  trait Capture[T[_[_]], C[_] <: ACopK] {
+    val C: Coalesce.Aux[T, C, C]
+    val F: Functor[C]
     val QC: UnicoalesceQC[T, C]
     val SR: UnicoalesceSR[T, C]
     val EJ: UnicoalesceEJ[T, C]
     val TJ: UnicoalesceTJ[T, C]
-    val N: Normalizable[C#M]
+    val N: Normalizable[C]
 
-    def run: C#M[T[C#M]] => C#M[T[C#M]] =
+    def run: C[T[C]] => C[T[C]] =
       apply[T, C](C, F, QC, SR, EJ, TJ, N)
   }
 
   object Capture {
 
-    implicit def materialize[T[_[_]], C <: CoM](
+    implicit def materialize[T[_[_]], C[_] <: ACopK](
       implicit
-        _C: Coalesce.Aux[T, C#M, C#M],
-        _F: Functor[C#M],
+        _C: Coalesce.Aux[T, C, C],
+        _F: Functor[C],
         _QC: UnicoalesceQC[T, C],
         _SR: UnicoalesceSR[T, C],
         _EJ: UnicoalesceEJ[T, C],
         _TJ: UnicoalesceTJ[T, C],
-        _N: Normalizable[C#M]): Capture[T, C] = new Capture[T, C] {
+        _N: Normalizable[C]): Capture[T, C] = new Capture[T, C] {
 
       val C = _C
       val F = _F
@@ -70,43 +70,43 @@ object Unicoalesce {
       val N = _N
     }
 
-    def apply[T[_[_]], C <: CoM](implicit C: Capture[T, C]) = C
+    def apply[T[_[_]], C[_] <: ACopK](implicit C: Capture[T, C]) = C
   }
 
-  def apply[T[_[_]], C <: CoM](
+  def apply[T[_[_]], C[_] <: ACopK](
     implicit
-      C: Coalesce.Aux[T, C#M, C#M],
-      F: Functor[C#M],
+      C: Coalesce.Aux[T, C, C],
+      F: Functor[C],
       QC: UnicoalesceQC[T, C],
       SR: UnicoalesceSR[T, C],
       EJ: UnicoalesceEJ[T, C],
       TJ: UnicoalesceTJ[T, C],
-      N: Normalizable[C#M]): C#M[T[C#M]] => C#M[T[C#M]] =
+      N: Normalizable[C]): C[T[C]] => C[T[C]] =
     repeatedly(
       applyTransforms(
         QC(C),
         SR(C),
         EJ(C),
         TJ(C),
-        N.normalizeF(_: C#M[T[C#M]])))
+        N.normalizeF(_: C[T[C]])))
 }
 
-sealed trait UnicoalesceQC[T[_[_]], C <: CoM] extends Unicoalesce[T, C]
+sealed trait UnicoalesceQC[T[_[_]], C[_] <: ACopK] extends Unicoalesce[T, C]
 
 private[qscript] trait UnicoalesceQCLowPriorityImplicits {
-  implicit def default[T[_[_]], C <: CoM]: UnicoalesceQC[T, C] = new UnicoalesceQC[T, C] {
-    def apply(C: Coalesce.Aux[T, C#M, C#M])(implicit F: Functor[C#M]) = κ(None)
+  implicit def default[T[_[_]], C[_] <: ACopK]: UnicoalesceQC[T, C] = new UnicoalesceQC[T, C] {
+    def apply(C: Coalesce.Aux[T, C, C])(implicit F: Functor[C]) = κ(None)
   }
 }
 
 object UnicoalesceQC extends UnicoalesceQCLowPriorityImplicits {
 
-  implicit def member[T[_[_]], C <: CoM](
+  implicit def member[T[_[_]], C[_] <: ACopK](
     implicit
-      QC: QScriptCore[T, ?] :<: C#M): UnicoalesceQC[T, C] = new UnicoalesceQC[T, C] {
+      QC: QScriptCore[T, ?] :<<: C): UnicoalesceQC[T, C] = new UnicoalesceQC[T, C] {
 
-    def apply(C: Coalesce.Aux[T, C#M, C#M])(implicit F: Functor[C#M]) =
-      C.coalesceQC[C#M](idPrism)
+    def apply(C: Coalesce.Aux[T, C, C])(implicit F: Functor[C]) =
+      C.coalesceQC[C](idPrism)
   }
 }
 
