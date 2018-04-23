fa2724f1237f02372b7b0f85f8c4f554072ac6d6
Łukasz Wawrzyk <lukasz.wawrzyk@gmail.com>
2018-04-23 15:36:16 +0200

    Fix repl

--- repl/src/main/scala/quasar/repl/Repl.scala
+++ repl/src/main/scala/quasar/repl/Repl.scala
@@ -52,9 +52,6 @@ import spire.std.double._
 object Repl {
   import Command.{XDir, XFile}
 
-  // TODO[scalaz]: Shadow the scalaz.Monad.monadMTMAB SI-2712 workaround
-  import EitherT.eitherTMonad
-
   val HelpMessage =
     """Quasar REPL, Copyright © 2014–2017 SlamData Inc.
       |
