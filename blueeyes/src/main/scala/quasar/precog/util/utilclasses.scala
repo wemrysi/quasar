b9f883b93b7ce378b514a9404bfaaaaaeed4172f
Alissa Pajer <alissa.pajer@gmail.com>
2018-05-14 18:16:19 -0600

    Delete unused VectorClock

--- blueeyes/src/main/scala/quasar/precog/util/utilclasses.scala
+++ blueeyes/src/main/scala/quasar/precog/util/utilclasses.scala
@@ -17,17 +17,12 @@
 package quasar.precog.util
 
 import quasar.blueeyes._
-import quasar.blueeyes.json.JValue
-import quasar.blueeyes.json.serialization.{Decomposer, Extractor}
-import quasar.blueeyes.json.serialization.DefaultSerialization._
-import quasar.blueeyes.json.serialization.Extractor._
 import quasar.time.OffsetDate
 
 import org.slf4s.Logging
 
 import scalaz._
 import scalaz.effect.IO
-import scalaz.Ordering.{LT, EQ, GT}
 
 import scala.annotation.tailrec
 import scala.collection.JavaConverters._
@@ -569,55 +564,3 @@ final class RingDeque[@specialized(Boolean, Int, Long, Double, Float, Short) A: 
     back = rotate(back, delta)
   }
 }
-
-
-
-
-case class VectorClock(map: Map[Int, Int]) {
-  def get(producerId: Int): Option[Int] = map.get(producerId)
-
-  def update(producerId: Int, sequenceId: Int): VectorClock =
-    if (map.get(producerId) forall { _ <= sequenceId }) {
-      VectorClock(map + (producerId -> sequenceId))
-    } else {
-      this
-    }
-
-  def isDominatedBy(other: VectorClock): Boolean = map forall {
-    case (prodId, maxSeqId) => other.get(prodId).forall(_ >= maxSeqId)
-  }
-}
-
-trait VectorClockSerialization {
-  implicit val VectorClockDecomposer: Decomposer[VectorClock] = new Decomposer[VectorClock] {
-    override def decompose(clock: VectorClock): JValue = clock.map.serialize
-  }
-
-  implicit val VectorClockExtractor: Extractor[VectorClock] = new Extractor[VectorClock] {
-    override def validated(obj: JValue): Validation[Error, VectorClock] =
-      (obj.validated[Map[Int, Int]]).map(VectorClock(_))
-  }
-}
-
-object VectorClock extends VectorClockSerialization {
-  def empty = apply(Map.empty)
-
-  implicit object order extends scalaz.Order[VectorClock] {
-    def order(c1: VectorClock, c2: VectorClock) =
-      if (c2.isDominatedBy(c1)) {
-        if (c1.isDominatedBy(c2)) EQ else GT
-      } else {
-        LT
-      }
-  }
-
-  // Computes the maximal merge of two clocks
-  implicit object semigroup extends Semigroup[VectorClock] {
-    def append(c1: VectorClock, c2: => VectorClock) = {
-      c2.map.foldLeft(c1) {
-        case (acc, (producerId, sequenceId)) => acc.update(producerId, sequenceId)
-      }
-    }
-  }
-}
-
