688128ad0a28eefaf8fb0c4f7ea4abac0d21c9af
Daniel Spiewak <daniel@slamdata.com>
2018-06-05 12:43:41 -0600

    Updated iota

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
     "com.chuusai"                %% "shapeless"                 % shapelessVersion,
     "org.scalacheck"             %% "scalacheck"                % scalacheckVersion,
     "com.propensive"             %% "contextual"                % "1.0.1",
-    "io.frees"                   %% "iotaz-core"                % "0.3.8-SNAPSHOT",
+    "io.frees"                   %% "iotaz-core"                % "0.3.8",
     "com.github.mpilquist"       %% "simulacrum"                % simulacrumVersion                    % Test,
     "org.typelevel"              %% "algebra-laws"              % algebraVersion                       % Test,
     "org.typelevel"              %% "discipline"                % disciplineVersion                    % Test,
