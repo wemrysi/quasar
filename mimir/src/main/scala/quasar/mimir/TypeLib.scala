5983bc388a6ad22dc65c3e77b1f78abd433c148b
Alissa Pajer <alissa.pajer@gmail.com>
2018-07-04 10:49:32 -0600

    Inline constant typeof string names

--- mimir/src/main/scala/quasar/mimir/TypeLib.scala
+++ mimir/src/main/scala/quasar/mimir/TypeLib.scala
@@ -34,37 +34,23 @@ trait TypeLibModule extends ColumnarTableLibModule {
       isArray)
 
     object typeOf extends Op1F1 {
-      val _number = "number"
-      val _boolean = "boolean"
-      val _string = "string"
-      val _null = "null"
-      val _offsetdatetime = "offsetdatetime"
-      val _offsetdate= "offsetdate"
-      val _offsettime = "offsettime"
-      val _localdatetime = "localdatetime"
-      val _localdate= "localdate"
-      val _localtime = "localtime"
-      val _interval = "interval"
-      val _emptyarray = "emptyarray"
-      val _emptyobject = "emptyobject"
-
       val tpe = UnaryOperationType(JType.JUniverseT, JTextT)
       def f1: F1 = CF1P {
-        case c: LongColumn => new ConstantStrColumn(c, _number)
-        case c: DoubleColumn => new ConstantStrColumn(c, _number)
-        case c: NumColumn => new ConstantStrColumn(c, _number)
-        case c: BoolColumn => new ConstantStrColumn(c, _boolean)
-        case c: StrColumn => new ConstantStrColumn(c, _string)
-        case c: NullColumn => new ConstantStrColumn(c, _null)
-        case c: OffsetDateTimeColumn => new ConstantStrColumn(c, _offsetdatetime)
-        case c: OffsetDateColumn => new ConstantStrColumn(c, _offsetdate)
-        case c: OffsetTimeColumn => new ConstantStrColumn(c, _offsettime)
-        case c: LocalDateTimeColumn => new ConstantStrColumn(c, _localdatetime)
-        case c: LocalDateColumn => new ConstantStrColumn(c, _localdate)
-        case c: LocalTimeColumn => new ConstantStrColumn(c, _localtime)
-        case c: IntervalColumn => new ConstantStrColumn(c, _interval)
-        case c: EmptyArrayColumn => new ConstantStrColumn(c, _emptyarray)
-        case c: EmptyObjectColumn => new ConstantStrColumn(c, _emptyobject)
+        case c: LongColumn => new ConstantStrColumn(c, "number")
+        case c: DoubleColumn => new ConstantStrColumn(c, "number")
+        case c: NumColumn => new ConstantStrColumn(c, "number")
+        case c: BoolColumn => new ConstantStrColumn(c, "boolean")
+        case c: StrColumn => new ConstantStrColumn(c, "string")
+        case c: NullColumn => new ConstantStrColumn(c, "null")
+        case c: OffsetDateTimeColumn => new ConstantStrColumn(c, "offsetdatetime")
+        case c: OffsetDateColumn => new ConstantStrColumn(c, "offsetdate")
+        case c: OffsetTimeColumn => new ConstantStrColumn(c, "offsettime")
+        case c: LocalDateTimeColumn => new ConstantStrColumn(c, "localdatetime")
+        case c: LocalDateColumn => new ConstantStrColumn(c, "localdate")
+        case c: LocalTimeColumn => new ConstantStrColumn(c, "localtime")
+        case c: IntervalColumn => new ConstantStrColumn(c, "interval")
+        case c: EmptyArrayColumn => new ConstantStrColumn(c, "emptyarray")
+        case c: EmptyObjectColumn => new ConstantStrColumn(c, "emptyobject")
       }
     }
 
