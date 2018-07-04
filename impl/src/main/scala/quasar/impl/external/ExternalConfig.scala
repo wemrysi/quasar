3bced4e7dd71e509f21087da93611e6d7a9ec17f
Emrys Ingersoll <ingersoll@gmail.com>
2018-07-04 10:30:25 -0500

    Replace 'BackendModule' references in ExternalConfig comments.

--- impl/src/main/scala/quasar/impl/external/ExternalConfig.scala
+++ impl/src/main/scala/quasar/impl/external/ExternalConfig.scala
@@ -49,9 +49,10 @@ object ExternalConfig {
   /**
    * A single directory containing plugin files, each of which will be
    * loaded as a backend. Plugin is defined by a json file containing
-   * a path to the main plugin jar from which `BackendModule` class
-   * name will be determined using the `Manifest.mf` file. The other thing
-   * in plugin file is a classpath that will be loaded for this plugin.
+   * a path to the main plugin jar from which `LightweightDataSourceModule`
+   * or `HeavyweightDataSourceModule` class name will be determined using the
+   * `Manifest.mf` file. The other thing in plugin file is a classpath that
+   * will be loaded for this plugin.
    */
   final case class PluginDirectory(dir: Path) extends ExternalConfig
 
@@ -59,7 +60,8 @@ object ExternalConfig {
    * Any files in the classpath will be loaded as jars; any directories
    * will be assumed to contain class files (e.g. the target output of
    * SBT compile).  The class name should be the fully qualified Java
-   * class name of the `BackendModule` implemented as a Scala object.
+   * class name of the `LightweightDataSourceModule` or
+   * `HeavyweightDataSourceModule` implemented as a Scala object.
    * In most cases, this means the class name here will end with a `$`
    */
   final case class ExplodedDirs(modules: List[(ClassName, ClassPath)]) extends ExternalConfig
