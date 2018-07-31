27ac03292030596653544eb730fd559e6bccb657
rintcius <rintcius@gmail.com>
2018-07-31 14:50:09 +0200

    also benchmark using forced tables

--- yggdrasilPerf/src/main/scala/quasar/yggdrasil/perf/LeftShiftBenchmark.scala
+++ yggdrasilPerf/src/main/scala/quasar/yggdrasil/perf/LeftShiftBenchmark.scala
@@ -33,12 +33,65 @@ import org.openjdk.jmh.infra.Blackhole
 import scalaz._, Scalaz._
 import shims._
 
+object LeftShiftBenchmark {
+  val P = new TestColumnarTableModule {}
+
+  def scrolling(nrKeysPerObject: Int, totalNrKeys: Int): Int => Option[RValue] = { r =>
+    assert(totalNrKeys >= nrKeysPerObject)
+    RObject(List.tabulate(nrKeysPerObject)(i => ("k" + ((r + i) % totalNrKeys)) -> CLong(i + r + 42)).toMap).some
+  }
+
+  val scalarUndefined: Int => Option[RValue] = { i =>
+    (i % 2) match {
+      case 0 => none
+      case _ => CLong(i + 42).some
+    }
+  }
+
+  def leftShiftRow(key: Int, value: Option[RValue]): RValue =
+    value match {
+      case None => RArray(CLong(key))
+      case Some(v) => RArray(CLong(key), v)
+    }
+
+  def leftShiftTestData(chunks: Int, chunkSize: Int, value: Int => Option[RValue]): Stream[List[RValue]] =
+    Stream.tabulate(chunks)(c =>
+      List.tabulate(chunkSize){ s =>
+        val i = (c * chunkSize) + s
+        leftShiftRow(i, value(i))
+      })
+
+  def mkTable(module: TestColumnarTableModule)(data: Stream[List[RValue]]): IO[module.Table] =
+    module.Table.fromRValueStream[WriterT[IO, List[IO[Unit]], ?]](
+      fs2.Stream.fromIterator[IO, fs2.Stream[IO, RValue]](
+        data.iterator.map(fs2.Stream.emits(_).covary[IO])
+      ).join).run.map(_._2)
+
+  abstract class BenchmarkState(chunks: Int, chunkSize: Int, v: Int => Option[RValue]) {
+    val table: IO[P.Table] = mkTable(P)(leftShiftTestData(chunks, chunkSize, v)).flatMap(_.force)
+  }
+
+  @State(Scope.Benchmark)
+  class BenchmarkState_1_10000_scrolling extends BenchmarkState(1, 10000, scrolling(nrKeysPerObject = 10, totalNrKeys = 14))
+
+  @State(Scope.Benchmark)
+  class BenchmarkState_5_10000_scrolling extends BenchmarkState(5, 10000, scrolling(nrKeysPerObject = 10, totalNrKeys = 14))
+
+  @State(Scope.Benchmark)
+  class BenchmarkState_1_10000_scalarUndefined extends BenchmarkState(1, 10000, scalarUndefined)
+
+  @State(Scope.Benchmark)
+  class BenchmarkState_1_1000000_scalarUndefined extends BenchmarkState(1, 1000000, scalarUndefined)
+
+  @State(Scope.Benchmark)
+  class BenchmarkState_5_10000_scalarUndefined extends BenchmarkState(5, 10000, scalarUndefined)
+}
+
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
 @BenchmarkMode(Array(Mode.AverageTime))
 @State(Scope.Benchmark)
 class LeftShiftBenchmark {
-
-  val P = new TestColumnarTableModule {}
+  import LeftShiftBenchmark._
 
   @Param(Array("5"))
   var chunks: Int = _
@@ -56,30 +109,17 @@ class LeftShiftBenchmark {
   @Param(Array("14"))
   var totalNrKeys: Int = _
 
-  def leftShiftRow(key: Int, value: Option[RValue]): RValue =
-    value match {
-      case None => RArray(CLong(key))
-      case Some(v) => RArray(CLong(key), v)
-    }
-
-  def leftShiftTestData(value: Int => Option[RValue]): Stream[List[RValue]] =
-    Stream.tabulate(chunks)(c =>
-      List.tabulate(chunkSize){ s =>
-        val i = (c * chunkSize) + s
-        leftShiftRow(i, value(i))
-      })
-
-  def mkTable(module: TestColumnarTableModule)(data: Stream[List[RValue]]): IO[module.Table] =
-    module.Table.fromRValueStream[WriterT[IO, List[IO[Unit]], ?]](
-      fs2.Stream.fromIterator[IO, fs2.Stream[IO, RValue]](
-        data.iterator.map(fs2.Stream.emits(_).covary[IO])
-      ).join).run.map(_._2)
-
   def leftShift(table: P.Table, emitOnUndef: Boolean): P.Table =
     table.leftShift(CPath.Identity \ 1, emitOnUndef)
 
   def doTest(v: Int => Option[RValue], bh: Blackhole): Unit = {
-    val table: IO[P.Table] = mkTable(P)(leftShiftTestData(v))
+    val table: IO[P.Table] = mkTable(P)(leftShiftTestData(chunks, chunkSize, v))
+    val tableAfter: IO[P.Table] = table.map(t => leftShift(t, true))
+    tableAfter.map(t => SliceTools.consumeTable(P)(t, bh)).unsafeRunSync
+  }
+
+  def doTestForce(state: BenchmarkState, bh: Blackhole): Unit = {
+    val table: IO[P.Table] = state.table
     val tableAfter: IO[P.Table] = table.map(t => leftShift(t, true))
     tableAfter.map(t => SliceTools.consumeTable(P)(t, bh)).unsafeRunSync
   }
@@ -108,11 +148,17 @@ class LeftShiftBenchmark {
 
   @Benchmark
   def scrollingFieldsObjects(bh: Blackhole): Unit = {
-    assert(totalNrKeys >= nrKeysPerObject)
-    val v: Int => Option[RValue] = { r =>
-      RObject(List.tabulate(nrKeysPerObject)(i => ("k" + ((r + i) % totalNrKeys)) -> CLong(i + r + 42)).toMap).some
-    }
-    doTest(v, bh)
+    doTest(scrolling(nrKeysPerObject, totalNrKeys), bh)
+  }
+
+  @Benchmark
+  def scrollingFieldsObjectsForced_1_10000(state: BenchmarkState_1_10000_scrolling, bh: Blackhole): Unit = {
+    doTestForce(state, bh)
+  }
+
+  @Benchmark
+  def scrollingFieldsObjectsForced_5_10000(state: BenchmarkState_5_10000_scrolling, bh: Blackhole): Unit = {
+    doTestForce(state, bh)
   }
 
   @Benchmark
@@ -130,12 +176,21 @@ class LeftShiftBenchmark {
 
   @Benchmark
   def scalarOrUndefined(bh: Blackhole): Unit = {
-    val v: Int => Option[RValue] = { i =>
-      (i % 2) match {
-        case 0 => none
-        case _ => CLong(i + 42).some
-      }
-    }
-    doTest(v, bh)
+    doTest(scalarUndefined, bh)
+  }
+
+  @Benchmark
+  def scalarOrUndefinedForced_1_10000(state: BenchmarkState_1_10000_scalarUndefined, bh: Blackhole): Unit = {
+    doTestForce(state, bh)
+  }
+
+  @Benchmark
+  def scalarOrUndefinedForced_1_1000000(state: BenchmarkState_1_1000000_scalarUndefined, bh: Blackhole): Unit = {
+    doTestForce(state, bh)
+  }
+
+  @Benchmark
+  def scalarOrUndefinedForced_5_10000(state: BenchmarkState_5_10000_scalarUndefined, bh: Blackhole): Unit = {
+    doTestForce(state, bh)
   }
 }
