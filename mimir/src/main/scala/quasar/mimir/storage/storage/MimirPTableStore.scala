c5c0697e4ee03ae41ea192c1f6264ce6fb1aebcc
Daniel Spiewak <daniel@slamdata.com>
2018-08-21 11:20:54 -0600

    Use `insertSegmentsVerified` in `MimirPTableStore`

--- mimir/src/main/scala/quasar/mimir/storage/storage/MimirPTableStore.scala
+++ mimir/src/main/scala/quasar/mimir/storage/storage/MimirPTableStore.scala
@@ -24,6 +24,7 @@ import quasar.contrib.pathy.{ADir, AFile}
 import quasar.contrib.scalaz.MonadError_
 import quasar.mimir.MimirCake.Cake
 import quasar.yggdrasil.ExactSize
+import quasar.yggdrasil.nihdb.SegmentsWrapper
 import quasar.yggdrasil.vfs.ResourceError
 import quasar.niflheim.NIHDB
 import quasar.yggdrasil.nihdb.NIHDBProjection
@@ -61,15 +62,16 @@ final class MimirPTableStore[F[_]: Monad: LiftIO] private (
       case Some((_, _, db)) =>
         fromStreamT(table.slices).zipWithIndex evalMap {
           case (slice, offset) =>
-            val jvs = slice.toJsonElements
-            IO.fromFutureShift(IO(db.insertVerified(NIHDB.Batch(offset, jvs.toList) :: Nil)))
+            val segments = SegmentsWrapper.sliceToSegments(offset.toLong, slice)
+            IO.fromFutureShift(
+              IO(db.insertSegmentsVerified(offset.toLong, slice.size, segments)))
         }
 
       case None =>
         Stream.empty
     }, {
       case Some((blob, version, db)) =>
-        IO.fromFutureShift(IO(db.cook)) *> cake.commitDB(blob, version, db)
+        cake.commitDB(blob, version, db)
 
       case None =>
         IO.pure(())
