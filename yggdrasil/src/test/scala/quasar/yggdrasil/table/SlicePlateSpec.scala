a9123fa23f23b802128b2827438a4544e25a15fa
Daniel Spiewak <daniel@slamdata.com>
2018-10-17 12:29:41 -0600

    Added support for parsing precise via SlicePlate

--- yggdrasil/src/test/scala/quasar/yggdrasil/table/SlicePlateSpec.scala
+++ yggdrasil/src/test/scala/quasar/yggdrasil/table/SlicePlateSpec.scala
@@ -18,20 +18,25 @@ package quasar.yggdrasil
 package table
 
 import quasar.blueeyes.json.JValue
+import quasar.common.data.{Data, DataGenerators}
+import quasar.frontend.data.DataCodec
 import quasar.precog.JsonTestSupport
+import quasar.precog.common.RValue
 
 import org.specs2.ScalaCheck
 import org.specs2.mutable.Specification
 
 import tectonic.json.Parser
 
-object SlicePlateSpec extends Specification with ScalaCheck {
+import scala.collection.immutable.ListMap
+
+object SlicePlateSpec extends Specification with ScalaCheck with DataGenerators {
   import JsonTestSupport._
 
   "slice plate parsing through tectonic" should {
-    "round trip" in prop { values: List[JValue] =>
+    "round trip readable" in prop { values: List[JValue] =>
       val input = values.mkString("\n")
-      val plate = new SlicePlate
+      val plate = new SlicePlate(false)
       val parser = Parser(plate, Parser.ValueStream)
 
       (parser.absorb(input), parser.finish()) must beLike {
@@ -40,5 +45,46 @@ object SlicePlateSpec extends Specification with ScalaCheck {
           results mustEqual values
       }
     }
+
+    "round trip precise" in prop { values0: List[Data] =>
+      val values = values0.map(sortFields(_)).flatMap(stripNA(_))
+
+      val input = values.flatMap(DataCodec.Precise.encode(_)).map(_.nospaces).mkString("\n")
+      val plate = new SlicePlate(true)
+      val parser = Parser(plate, Parser.ValueStream)
+
+      (parser.absorb(input), parser.finish()) must beLike {
+        case (Right(slices1), Right(slices2)) =>
+          val results = (slices1 ++ slices2).flatMap(_.toRValues).map(RValue.toData(_)).map(sortFields(_))
+          results mustEqual values
+      }
+    }
+  }
+
+  private[this] def sortFields(data: Data): Data = data match {
+    case Data.Obj(fields) =>
+      val fields2 = ListMap(fields.mapValues(sortFields(_)).toList.sortBy(_._1): _*)
+      Data.Obj(fields2)
+
+    case Data.Arr(values) =>
+      Data.Arr(values.map(sortFields(_)))
+
+    case other => other
+  }
+
+  private[this] def stripNA(data: Data): Option[Data] = data match {
+    case Data.Obj(fields) =>
+      val fields2 = fields flatMap {
+        case (key, value) => stripNA(value).map(key -> _)
+      }
+
+      Some(Data.Obj(fields2))
+
+    case Data.Arr(values) =>
+      Some(Data.Arr(values.flatMap(stripNA(_))))
+
+    case Data.NA => None
+
+    case other => Some(other)
   }
 }
