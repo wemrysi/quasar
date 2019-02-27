a78ac8a687bad9cd5313ec2a65d345c32ea24fff
Emrys Ingersoll <ingersoll@gmail.com>
2019-02-27 10:05:55 -0800

    QSU updates to work with refactored provenance

--- qscript/src/test/scala/quasar/qscript/provenance/DimensionSpec.scala
+++ qscript/src/test/scala/quasar/qscript/provenance/DimensionSpec.scala
@@ -42,52 +42,40 @@ object DimensionSpec extends Qspec {
   val T: Boolean = true
   val F: Boolean = false
 
+  val B: Fix[ProvF[Char, I, Boolean, ?]] =
+    P.fresh()
+
+  val Base: Dimensions[Fix[ProvF[Char, I, Boolean, ?]]] =
+    Dimensions.origin(B)
+
   def ikey(l: Int, r: Int): JoinKey[I] =
     JoinKey(i(l), i(r))
 
   import P.implicits._
-
-  "project path on empty dims creates new" >> {
-    D.projectPath('q', D.empty) must_= Dimensions.origin(P.prjPath('q'))
-  }
 
   "project static on empty dims is empty" >> {
     D.projectStatic('x', T, D.empty) must_= D.empty
   }
 
   "project static key known not to exist is empty" >> {
-    val d =
-      D.injectStatic(
-        'y', T,
-        D.lshift(
-          i(1), T,
-          D.projectStatic(
-            'c', T,
-            D.projectPath('f', D.empty))))
+    val d = D.injectStatic('y', T, D.lshift(i(1), T, D.projectStatic('c', T, Base)))
 
     D.projectStatic('z', T, d) must_= D.empty
   }
 
   "project static key of different sort is empty" >> {
-    val d =
-      D.injectStatic(
-        'y', T,
-        D.lshift(
-          i(1), T,
-          D.projectStatic(
-            'c', T,
-            D.projectPath('f', D.empty))))
+    val d = D.injectStatic('y', T, D.lshift(i(1), T, D.projectStatic('c', T, Base)))
 
     D.projectStatic('y', F, d) must_= D.empty
   }
 
   "project static eliminates inject static" >> {
     val init = Dimensions.origin(
-      P.both(P.injValue('x', F), P.injValue('y', T)),
-      P.value(i(3), T),
-      P.prjPath('a'))
+      P.both(P.inject('x', F), P.inject('y', T)),
+      P.inflate(i(3), T),
+      B)
 
-    val exp = Dimensions.origin(P.value(i(3), T), P.prjPath('a'))
+    val exp = Dimensions.origin(P.inflate(i(3), T), B)
 
     D.projectStatic('x', F, init) must_= exp
   }
@@ -95,11 +83,11 @@ object DimensionSpec extends Qspec {
   "project static eliminates inject static within sequence" >> {
     val init = Dimensions.origin(
       P.thenn(
-        P.both(P.injValue('x', T), P.injValue('y', F)),
-        P.value(i(3), T)),
-      P.prjPath('a'))
+        P.both(P.inject('x', T), P.inject('y', F)),
+        P.inflate(i(3), T)),
+      B)
 
-    val exp = Dimensions.origin(P.value(i(3), T), P.prjPath('a'))
+    val exp = Dimensions.origin(P.inflate(i(3), T), B)
 
     D.projectStatic('x', T, init) must_= exp
   }
@@ -110,8 +98,8 @@ object DimensionSpec extends Qspec {
 
   "autojoin keys" >> {
     "builds join keys from zipped dimensions" >> {
-      val x = D.lshift(i(1), F, D.lshift(i(2), T, D.projectPath('a', D.empty)))
-      val y = D.lshift(i(3), F, D.lshift(i(4), T, D.projectPath('a', D.empty)))
+      val x = D.lshift(i(1), F, D.lshift(i(2), T, Base))
+      val y = D.lshift(i(3), F, D.lshift(i(4), T, Base))
 
       val jks = JoinKeys(IList(NonEmptyList(
         NonEmptyList(ikey(1, 3)),
@@ -121,8 +109,8 @@ object DimensionSpec extends Qspec {
     }
 
     "flatten joins with shift of same sort" >> {
-      val x = D.lshift(i(3), T, D.flatten(i(2), T, D.lshift(i(1), T, D.projectPath('a', D.empty))))
-      val y = D.lshift(i(9), T, D.lshift(i(8), T, D.lshift(i(7), T, D.projectPath('a', D.empty))))
+      val x = D.lshift(i(3), T, D.flatten(i(2), T, D.lshift(i(1), T, Base)))
+      val y = D.lshift(i(9), T, D.lshift(i(8), T, D.lshift(i(7), T, Base)))
 
       val jks = JoinKeys(IList(NonEmptyList(
         NonEmptyList(ikey(1, 7)),
@@ -133,8 +121,8 @@ object DimensionSpec extends Qspec {
     }
 
     "flatten crosses with shift of different sort" >> {
-      val x = D.lshift(i(3), T, D.flatten(i(2), F, D.lshift(i(1), T, D.projectPath('a', D.empty))))
-      val y = D.lshift(i(9), T, D.lshift(i(8), T, D.lshift(i(7), T, D.projectPath('a', D.empty))))
+      val x = D.lshift(i(3), T, D.flatten(i(2), F, D.lshift(i(1), T, Base)))
+      val y = D.lshift(i(9), T, D.lshift(i(8), T, D.lshift(i(7), T, Base)))
 
       val jks = JoinKeys(IList(NonEmptyList(NonEmptyList(ikey(1, 7)))))
 
@@ -142,8 +130,8 @@ object DimensionSpec extends Qspec {
     }
 
     "flatten joins with project of same sort" >> {
-      val x = D.lshift(i(3), T, D.projectStatic('k', T, D.lshift(i(1), T, D.projectPath('a', D.empty))))
-      val y = D.flatten(i(9), T, D.lshift(i(7), T, D.projectPath('a', D.empty)))
+      val x = D.lshift(i(3), T, D.projectStatic('k', T, D.lshift(i(1), T, Base)))
+      val y = D.flatten(i(9), T, D.lshift(i(7), T, Base))
 
       val jks = JoinKeys(IList(NonEmptyList(
         NonEmptyList(ikey(1, 7)),
@@ -153,8 +141,8 @@ object DimensionSpec extends Qspec {
     }
 
     "flatten crosses with project of different sort" >> {
-      val x = D.lshift(i(3), T, D.projectStatic('k', F, D.lshift(i(1), T, D.projectPath('a', D.empty))))
-      val y = D.flatten(i(9), T, D.lshift(i(7), T, D.projectPath('a', D.empty)))
+      val x = D.lshift(i(3), T, D.projectStatic('k', F, D.lshift(i(1), T, Base)))
+      val y = D.flatten(i(9), T, D.lshift(i(7), T, Base))
 
       val jks = JoinKeys(IList(NonEmptyList(NonEmptyList(ikey(1, 7)))))
 
@@ -162,8 +150,8 @@ object DimensionSpec extends Qspec {
     }
 
     "shift joins with project of same sort" >> {
-      val x = D.lshift(i(3), T, D.projectStatic('k', T, D.lshift(i(1), T, D.projectPath('a', D.empty))))
-      val y = D.lshift(i(9), T, D.lshift(i(7), T, D.projectPath('a', D.empty)))
+      val x = D.lshift(i(3), T, D.projectStatic('k', T, D.lshift(i(1), T, Base)))
+      val y = D.lshift(i(9), T, D.lshift(i(7), T, Base))
 
       val jks = JoinKeys(IList(NonEmptyList(
         NonEmptyList(ikey(1, 7)),
@@ -173,8 +161,8 @@ object DimensionSpec extends Qspec {
     }
 
     "shift crosses with project of different sort" >> {
-      val x = D.lshift(i(3), T, D.projectStatic('k', T, D.lshift(i(1), T, D.projectPath('a', D.empty))))
-      val y = D.lshift(i(9), F, D.lshift(i(7), T, D.projectPath('a', D.empty)))
+      val x = D.lshift(i(3), T, D.projectStatic('k', T, D.lshift(i(1), T, Base)))
+      val y = D.lshift(i(9), F, D.lshift(i(7), T, Base))
 
       val jks = JoinKeys(IList(NonEmptyList(NonEmptyList(ikey(1, 7)))))
 
@@ -182,8 +170,8 @@ object DimensionSpec extends Qspec {
     }
 
     "halts the join on project mismatch" >> {
-      val x = D.lshift(i(2), T, D.projectStatic('x', T, D.flatten(i(1), T, D.projectPath('a', D.empty))))
-      val y = D.lshift(i(8), T, D.projectStatic('y', T, D.flatten(i(7), T, D.projectPath('a', D.empty))))
+      val x = D.lshift(i(2), T, D.projectStatic('x', T, D.flatten(i(1), T, Base)))
+      val y = D.lshift(i(8), T, D.projectStatic('y', T, D.flatten(i(7), T, Base)))
 
       val jks = JoinKeys(IList(NonEmptyList(NonEmptyList(ikey(1, 7)))))
 
@@ -191,25 +179,18 @@ object DimensionSpec extends Qspec {
     }
 
     "halts the join on sort mismatch" >> {
-      val x = D.lshift(i(2), T, D.projectStatic('y', F, D.flatten(i(1), T, D.projectPath('a', D.empty))))
-      val y = D.lshift(i(8), T, D.projectStatic('y', T, D.flatten(i(7), T, D.projectPath('a', D.empty))))
+      val x = D.lshift(i(2), T, D.projectStatic('y', F, D.flatten(i(1), T, Base)))
+      val y = D.lshift(i(8), T, D.projectStatic('y', T, D.flatten(i(7), T, Base)))
 
       val jks = JoinKeys(IList(NonEmptyList(NonEmptyList(ikey(1, 7)))))
 
       D.autojoinKeys(x, y) must_= jks
     }
 
-    "halts the join on path mismatch" >> {
-      val x = D.lshift(i(2), T, D.flatten(i(1), T, D.projectPath('b', D.projectPath('a', D.empty))))
-      val y = D.lshift(i(8), T, D.flatten(i(7), T, D.projectPath('c', D.projectPath('a', D.empty))))
-
-      D.autojoinKeys(x, y) must_= JoinKeys.empty
-    }
-
     "join with union disjoins keys for each side" >> {
-      val x = D.lshift(i(2), T, D.projectPath('a', D.empty))
-      val y = D.lshift(i(3), T, D.projectPath('a', D.empty))
-      val z = D.lshift(i(4), T, D.projectPath('a', D.empty))
+      val x = D.lshift(i(2), T, Base)
+      val y = D.lshift(i(3), T, Base)
+      val z = D.lshift(i(4), T, Base)
 
       val jks = JoinKeys(IList(
         NonEmptyList(NonEmptyList(ikey(2, 4))),
