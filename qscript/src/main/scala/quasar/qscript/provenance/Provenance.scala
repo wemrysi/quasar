57ecde21782cb57bcd787354db8c5b05e5a97e40
Emrys Ingersoll <ingersoll@gmail.com>
2019-03-18 20:26:44 -0500

    Provenance module

---
+++ qscript/src/main/scala/quasar/qscript/provenance/Provenance.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2014–2018 SlamData Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package quasar.qscript.provenance
+
+trait Provenance[S, V, T] {
+  type P
+
+  def autojoinKeys(l: P, r: P): JoinKeys[S, V]
+
+  /** The conjunction of two provenance. */
+  def and(l: P, r: P): P
+
+  /** Provenance having a dimensionality of zero. */
+  def empty: P
+
+  /** Append an identity, increasing dimensionality by 1. */
+  def inflate(vectorId: V, sort: T, p: P): P
+
+  /** Append an identity, maintaining current dimensionality. */
+  def inflateConjoin(vectorId: V, sort: T, p: P): P
+
+  /** "Submerge" an identity, making it the second-highest dimension,
+    * increasing dimensionality by 1.
+    */
+  def inflateSubmerge(vectorId: V, sort: T, p: P): P
+
+  /** Inject into a structure at an unknown field. */
+  val injectDynamic(p: P): P
+
+  /** Inject into a structure at the given field. */
+  def injectStatic(scalarId: S, sort: T, p: P): P
+
+  /** The disjunction of two provenance. */
+  def or(l: P, r: P): P
+
+  /** Project an unknown field. */
+  val projectDynamic(p: P): P
+
+  /** Project a statically known field. */
+  def projectStatic(scalarId: S, sort: T, p: P): P
+
+  /** Discard the highest dimension. */
+  def reduce(p: P): P
+
+  // Optics
+
+  def scalarIds: Traversal[P, (S, T)]
+
+  def vectorIds: Traversal[P, (V, T)]
+}
+
+object Provenance {
+  type Aux[S, V, T, P0] = Provenance[S, V, T] { P = P0 }
+}
