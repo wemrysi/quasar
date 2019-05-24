b2ca03ddb87a6cee108bd35d0e9520646824f2ad
Maxim Zimaliev <zimaliev@gmail.com>
2019-05-24 18:48:28 +0300

    removed event stream stuff, some golfing, bracket wrapping iterator

--- impl/src/main/scala/quasar/impl/storage/AsyncDistributedMapStore.scala
+++ impl/src/main/scala/quasar/impl/storage/AsyncDistributedMapStore.scala
@@ -19,16 +19,14 @@ package quasar.impl.storage
 import slamdata.Predef._
 
 import cats.effect._
-import cats.effect.concurrent.Deferred
 import cats.syntax.functor._
 import cats.syntax.applicative._
 import cats.syntax.apply._
 import cats.syntax.flatMap._
 
-import fs2.concurrent.Queue
 import fs2.Stream
 
-import io.atomix.core.map.{AsyncDistributedMap, MapEvent, MapEventListener}, MapEvent.Type._
+import io.atomix.core.map.AsyncDistributedMap
 import io.atomix.core.iterator.AsyncIterator
 
 import java.util.concurrent.CompletableFuture
@@ -40,7 +38,8 @@ final class AsyncDistributedMapStore[F[_]: Async: ContextShift, K, V](
   import AsyncDistributedMapStore._
 
   def entries: Stream[F, (K, V)] = for {
-    iterator <- Stream.eval(Sync[F].delay(mp.entrySet.iterator))
+    iterator <- Stream.bracket(Sync[F].delay(mp.entrySet.iterator))(
+      (it: AsyncIterator[java.util.Map.Entry[K, V]]) => toF(it.close()) as (()))
     entry <- fromIterator[F, java.util.Map.Entry[K, V]](iterator)
   } yield (entry.getKey, entry.getValue)
 
@@ -51,31 +50,10 @@ final class AsyncDistributedMapStore[F[_]: Async: ContextShift, K, V](
     toF(mp.put(k, v)) as (())
 
   def delete(k: K): F[Boolean] =
-    toF(mp.remove(k)) map { (x: V) => !Option(x).isEmpty }
+    toF(mp.remove(k)) map { (x: V) => Option(x).nonEmpty }
 }
 
 object AsyncDistributedMapStore {
-  trait IndexedStoreEvent[K, +V]
-
-  final case class Insert[K, V](k: K, v: V) extends IndexedStoreEvent[K, V]
-  final case class Remove[K](k: K) extends IndexedStoreEvent[K, Nothing]
-
-  def eventStream[F[_]: ConcurrentEffect: ContextShift, K, V](mp: AsyncDistributedMap[K, V]): Stream[F, IndexedStoreEvent[K, V]] = {
-    val F = ConcurrentEffect[F]
-    def run(f: F[Unit]): Unit = F.runAsync(f)(_ => IO.unit).unsafeRunSync
-    def listener(cb: IndexedStoreEvent[K, V] => Unit): MapEventListener[K, V] = { event => event.`type` match {
-      case INSERT => cb(Insert(event.key, event.newValue))
-      case UPDATE => cb(Insert(event.key, event.newValue))
-      case REMOVE => cb(Remove(event.key))
-    }}
-    for {
-      q <- Stream.eval(Queue.bounded[F, IndexedStoreEvent[K, V]](1))
-      handler = listener(x => run(q.enqueue1(x)))
-      _ <- Stream.eval(toF(mp addListener handler))
-      res <- q.dequeue.onFinalize(toF(mp removeListener handler) as (()))
-    } yield res
-  }
-
   def fromIterator[F[_]: ContextShift: Async, A](iterator: AsyncIterator[A]): Stream[F, A] = {
     def getNext(i: AsyncIterator[A]): F[Option[(A, AsyncIterator[A])]] = for {
       hasNext <- toF(i.hasNext())
@@ -85,30 +63,11 @@ object AsyncDistributedMapStore {
   }
 
   def toF[F[_]: Async: ContextShift, A](cf: CompletableFuture[A]): F[A] = {
-    if (cf.isDone) cf.get.pure[F]
-    else {
-      val fa: F[A] = Async[F].async { cb: (Either[Throwable, A] => Unit) =>
-        val _ = cf.whenComplete { (res: A, t: Throwable) => {
-          val eitherResult: Either[Throwable, A] =
-            if (!Option(t).isEmpty) Left(t) else Right(res)
-          cb(eitherResult)
-        }}
-      }
-      fa productL ContextShift[F].shift
-    }
-  }
-
-  def consume[F[_]: Concurrent, K, V](
-      stream: Stream[F, IndexedStoreEvent[K, V]],
-      store: IndexedStore[F, K, V])
-      : F[Deferred[F, Unit]] = {
-    def handleEvent(e: IndexedStoreEvent[K, V]): F[Unit] = e match {
-      case Insert(k, v) => store.insert(k, v)
-      case Remove(k) => store.delete(k) as (())
-    }
-    for {
-      d <- Deferred[F, Unit]
-      _ <- Concurrent[F].start(stream.onFinalize(d.complete(())).evalMap(handleEvent).compile.drain)
-    } yield d
+    if (cf.isDone)
+      cf.get.pure[F]
+    else
+      Async[F].async { cb: (Either[Throwable, A] => Unit) =>
+        val _ = cf.whenComplete { (res: A, t: Throwable) => cb(Option(t).toLeft(res)) }
+      } productL ContextShift[F].shift
   }
 }
