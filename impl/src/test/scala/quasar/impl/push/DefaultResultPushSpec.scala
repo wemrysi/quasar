cd8a4eaa6512e1bc85dbb152fe147cd9c43aa8fc
Juan Pablo Santos <santos.juanpablo@gmail.com>
2019-08-08 20:53:23 -0500

    Test result push cancellation

--- impl/src/test/scala/quasar/impl/push/DefaultResultPushSpec.scala
+++ impl/src/test/scala/quasar/impl/push/DefaultResultPushSpec.scala
@@ -18,15 +18,15 @@ package quasar.impl.push
 
 import slamdata.Predef._
 
-import quasar.{ConditionMatchers, EffectfulQSpec}
 import quasar.api.QueryEvaluator
 import quasar.api.destination.DestinationType
 import quasar.api.destination.ResultType
 import quasar.api.push.ResultPush
+import quasar.api.resource.ResourcePath
 import quasar.api.resource.{ResourcePath, ResourceName}
 import quasar.api.table.{TableColumn, TableName, TableRef}
 import quasar.connector.{Destination, ResultSink}
-import quasar.api.resource.ResourcePath
+import quasar.{ConditionMatchers, EffectfulQSpec}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
@@ -37,24 +37,26 @@ import eu.timepit.refined.auto._
 import fs2.concurrent.SignallingRef
 import fs2.job.JobManager
 import fs2.{Stream, text}
-import scalaz.{Equal, NonEmptyList}
 import scalaz.std.set._
 import scalaz.std.string._
+import scalaz.{Equal, NonEmptyList}
 
 object DefaultResultPushSpec extends EffectfulQSpec[IO] with ConditionMatchers {
   implicit val tmr = IO.timer(global)
 
   val TableId = 42
-  val DestinationId = 43
+  val DestinationId = 42
   val RefDestinationType = DestinationType("ref", 1L)
 
-  def streamToString(s: Stream[IO, Byte]): IO[String] =
-    s.through(text.utf8Decode).compile.toList.map(_.mkString)
+  // convert a bytestream into stream of space separeted words
+  def bytesToString(s: Stream[IO, Byte]): Stream[IO, String] =
+    s.split(_ == 0x20).flatMap(Stream.chunk).through(text.utf8Decode)
 
   type Filesystem = Map[ResourcePath, String]
 
   val emptyFilesystem: Filesystem = Map.empty[ResourcePath, String]
 
+  // a sink that writes to a Ref
   final class RefCsvSink(ref: Ref[IO, Filesystem]) extends ResultSink[IO] {
     type RT = ResultType.Csv[IO]
 
@@ -63,10 +65,10 @@ object DefaultResultPushSpec extends EffectfulQSpec[IO] with ConditionMatchers {
     def apply(dst: ResourcePath, result: (List[TableColumn], Stream[IO, Byte])): IO[Unit] = {
       val (columns, data) = result
 
-      for {
-        stringData <- streamToString(data)
-        update <- ref.update(currentFs => (currentFs + (dst -> stringData)))
-      } yield update
+      // write stream one word at a time to ref
+      bytesToString(data).evalMap(str =>
+        ref.update(currentFs =>
+          currentFs + (dst -> currentFs.get(dst).fold(str)(_ ++ str)))).compile.drain
     }
   }
 
@@ -117,7 +119,7 @@ object DefaultResultPushSpec extends EffectfulQSpec[IO] with ConditionMatchers {
   "result push" >> {
     "push a table to a destination" >>* {
       val pushPath = ResourcePath.root() / ResourceName("foo") / ResourceName("bar")
-      val query = "query something"
+      val query = "query"
       val testTable = TableRef(TableName("foo"), query, List())
 
       for {
@@ -128,7 +130,7 @@ object DefaultResultPushSpec extends EffectfulQSpec[IO] with ConditionMatchers {
         evaluator = mkEvaluator(q => Stream.emit(mockEvaluate(q)) ++ Stream.eval_(ref.set("Finished")))
 
         push = mkResultPush(testTable, destination, jm, evaluator)
-        startRes <- push.start(42, 42, pushPath, ResultType.Csv[IO](), None)
+        startRes <- push.start(TableId, DestinationId, pushPath, ResultType.Csv[IO](), None)
         _ <- latchGet(ref, "Finished")
         filesystemAfterPush <- filesystem.get
         _ <- cleanup
@@ -141,11 +143,11 @@ object DefaultResultPushSpec extends EffectfulQSpec[IO] with ConditionMatchers {
 
     "cancel a table push" >>* {
       val pushPath = ResourcePath.root() / ResourceName("foo") / ResourceName("bar")
-      val query = "query something"
+      val query = "query"
       val testTable = TableRef(TableName("foo"), query, List())
 
       def testStream(ref: SignallingRef[IO, String]): Stream[IO, String] =
-        Stream.eval(ref.set("Started")).as("foo") ++ Stream.sleep_(Duration(100, MILLISECONDS)) ++ Stream.eval(ref.set("Finished")).as("bar")
+        Stream.eval(ref.set("Started")).as("foo ") ++ Stream.sleep_(Duration(100, MILLISECONDS)) ++ Stream.eval(ref.set("Finished")).as("bar")
 
       for {
         filesystem <- Ref.of[IO, Filesystem](emptyFilesystem)
@@ -153,14 +155,16 @@ object DefaultResultPushSpec extends EffectfulQSpec[IO] with ConditionMatchers {
         destination = new RefDestination(filesystem)
         ref <- SignallingRef[IO, String]("Not started")
         push = mkResultPush(testTable, destination, jm, mkEvaluator(_ => testStream(ref)))
-        startRes <- push.start(42, 42, pushPath, ResultType.Csv[IO](), None)
+        startRes <- push.start(TableId, DestinationId, pushPath, ResultType.Csv[IO](), None)
         _ <- latchGet(ref, "Started")
         cancelRes <- push.cancel(42)
         filesystemAfterPush <- filesystem.get
         refAfter <- ref.get
         _ <- cleanup
       } yield {
-        filesystemAfterPush.keySet must equal(Set.empty[ResourcePath])
+        filesystemAfterPush.keySet must equal(Set(pushPath))
+        // check if a *partial* result was pushed
+        filesystemAfterPush(pushPath) must equal("foo")
         startRes must beNormal
         cancelRes must beNormal
         refAfter must equal("Started")
