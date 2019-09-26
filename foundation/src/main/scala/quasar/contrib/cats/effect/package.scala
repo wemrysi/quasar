1bb079aeefe1a8b5440aa0754c6b01720f00ce51
Daniel Spiewak <daniel@slamdata.com>
2019-09-26 11:59:11 -0600

    Avoid leaking threads when futures error

--- foundation/src/main/scala/quasar/contrib/cats/effect/package.scala
+++ foundation/src/main/scala/quasar/contrib/cats/effect/package.scala
@@ -35,6 +35,6 @@ package object effect {
 
   implicit class IOOps(val self: IO.type) extends AnyVal {
     def fromFutureShift[A](iofa: IO[Future[A]])(implicit cs: ContextShift[IO]): IO[A] =
-      IO.fromFuture(iofa).flatMap(a => IO.shift.as(a))
+      IO.fromFuture(iofa).guarantee(IO.shift)
    }
 }
