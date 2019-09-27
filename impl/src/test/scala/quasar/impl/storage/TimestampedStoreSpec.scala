3260cbb27e748ddc5f36c5ee177ba596f1e56980
Maxim Zimaliev <zimaliev@gmail.com>
2019-09-27 15:17:17 +0300

    added updates stream test

--- impl/src/test/scala/quasar/impl/storage/TimestampedStoreSpec.scala
+++ impl/src/test/scala/quasar/impl/storage/TimestampedStoreSpec.scala
@@ -22,6 +22,7 @@ import quasar.concurrent.BlockingContext
 import quasar.impl.cluster.Timestamped
 
 import cats.effect.{IO, Resource, Timer}
+import cats.effect.concurrent.{Ref, Deferred}
 
 import scalaz.std.string._
 
@@ -117,6 +118,27 @@ final class TimestampedStoreSpec extends IndexedStoreSpec[IO, String, String] {
         timestamps.get("bar") mustEqual(bar.map(Timestamped.timestamp(_)))
       }
       resource.use(IO.pure(_))
+    }
+    "updates stream works" >>* {
+      for {
+        (ts, finish) <- underlying.flatMap(TimestampedStore(_)).allocated
+        ref <- Ref.of[IO, List[(String, Timestamped[String])]](List())
+        streamIsFinished <- Deferred[IO, Unit]
+        _ <- ts.updates.evalMap(x => ref.modify(lst => (x :: lst, ()))).onFinalize(streamIsFinished.complete(())).compile.drain.start
+        _ <- ts.insert("foo", "bar")
+        _ <- ts.delete("foo")
+        _ <- ts.insert("foo", "baz")
+        _ <- ts.insert("bar", "foo")
+        _ <- finish
+        // the stream is finished gracefully (it handles leftover updates from queue instead of force termination)
+        // so we need to wait for them
+        _ <- streamIsFinished.get
+        lst <- ref.get.map(_.reverse)
+      } yield {
+        lst.size mustEqual 4
+        lst.map(_._1) mustEqual List("foo", "foo", "foo", "bar")
+        lst.map(x => Timestamped.raw(x._2)) mustEqual List(Some("bar"), None, Some("baz"), Some("foo"))
+      }
     }
   }
 }
