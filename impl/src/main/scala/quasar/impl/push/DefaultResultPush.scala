35fcb78633284f19a25b66838130d8231de5acbd
Emrys Ingersoll <ingersoll@gmail.com>
2019-11-07 09:11:50 -0600

    A more destination-centric ResultPush
    * Adds a derived method to bulk-start pushes
    * Allows mutlple pushes of a table to different destinations
    * Replaces per-table status with status of all pushes to a destination

--- impl/src/main/scala/quasar/impl/push/DefaultResultPush.scala
+++ impl/src/main/scala/quasar/impl/push/DefaultResultPush.scala
@@ -16,7 +16,7 @@
 
 package quasar.impl.push
 
-import slamdata.Predef.{Map => _, _}
+import slamdata.Predef._
 
 import quasar.Condition
 import quasar.api.destination.{Destination, ResultSink}
@@ -27,53 +27,51 @@ import quasar.api.resource.ResourcePath
 import quasar.api.table.TableRef
 
 import java.time.Instant
-import java.util.Map
+import java.util.{Map => JMap}
 import java.util.concurrent.ConcurrentHashMap
 
+import scala.collection.JavaConverters._
 import scala.concurrent.duration.{FiniteDuration, MILLISECONDS}
 
+import cats.data.{EitherT, OptionT}
 import cats.effect.{Concurrent, Timer}
+import cats.implicits._
+
 import fs2.Stream
 import fs2.job.{JobManager, Job, Event => JobEvent}
-import scalaz.std.list._
-import scalaz.std.option._
-import scalaz.syntax.either._
-import scalaz.syntax.foldable._
-import scalaz.syntax.monad._
-import scalaz.syntax.show._
-import scalaz.syntax.std.option._
-import scalaz.{EitherT, Functor, Id, NonEmptyList, OptionT, Traverse, \/}
 
-import shims.monadToScalaz
+import shims.showToCats
 
-class DefaultResultPush[
-  F[_]: Concurrent: Timer, T, D, Q, R] private (
+class DefaultResultPush[F[_]: Concurrent: Timer, T, D, Q, R] private (
     lookupTable: T => F[Option[TableRef[Q]]],
     evaluator: QueryEvaluator[F, Q, Stream[F, R]],
     lookupDestination: D => F[Option[Destination[F]]],
-    jobManager: JobManager[F, T, Nothing],
+    jobManager: JobManager[F, (D, T), Nothing],
     render: ResultRender[F, R],
-    pushStatus: Map[T, PushMeta[D]])
+    pushStatus: JMap[D, JMap[T, PushMeta]])
     extends ResultPush[F, T, D] {
+
   import ResultPushError._
 
   def start(tableId: T, destinationId: D, path: ResourcePath, format: ResultType, limit: Option[Long])
       : F[Condition[ResultPushError[T, D]]] = {
 
+    type RPE = ResultPushError[T, D]
+
     val writing = for {
-      dest <- liftOptionF[F, ResultPushError[T, D], Destination[F]](
+      dest <- EitherT.fromOptionF[F, RPE, Destination[F]](
         lookupDestination(destinationId),
         ResultPushError.DestinationNotFound(destinationId))
 
-      tableRef <- liftOptionF[F, ResultPushError[T, D], TableRef[Q]](
+      tableRef <- EitherT.fromOptionF[F, RPE, TableRef[Q]](
         lookupTable(tableId),
         ResultPushError.TableNotFound(tableId))
 
       sink <- format match {
         case ResultType.Csv =>
-          liftOptionF[F, ResultPushError[T, D], ResultSink.Csv[F]](
-            findCsvSink(dest.sinks).point[F],
-            ResultPushError.FormatNotSupported(destinationId, format.shows))
+          EitherT.fromOptionF[F, RPE, ResultSink.Csv[F]](
+            findCsvSink(dest.sinks).pure[F],
+            ResultPushError.FormatNotSupported(destinationId, format.show))
       }
 
       query = tableRef.query
@@ -81,104 +79,150 @@ class DefaultResultPush[
 
       evaluated = format match {
         case ResultType.Csv =>
-          evaluator.evaluate(query).map(_.flatMap(render.renderCsv(_, columns, sink.config, limit)))
+          evaluator.evaluate(query)
+            .map(_.flatMap(render.renderCsv(_, columns, sink.config, limit)))
       }
 
       sinked = sink.run(path, columns, Stream.force(evaluated)).map(Right(_))
 
-      now <- EitherT.rightT(instantNow)
-      submitted <- EitherT.rightT(jobManager.submit(Job(tableId, sinked)))
-      _ <- EitherT.either[F, ResultPushError[T, D], Unit](
-        if (submitted)
-          ().right
-        else
-          ResultPushError.PushAlreadyRunning(tableId).left)
+      now <- EitherT.right[RPE](instantNow)
+      submitted <- EitherT.right[RPE](jobManager.submit(Job((destinationId, tableId), sinked)))
 
-      pushMeta = PushMeta(destinationId, path, format, Status.running(now), limit)
-      _ <- EitherT.rightT(Concurrent[F].delay(pushStatus.put(tableId, pushMeta)))
+      _ <- EitherT.cond[F](
+        submitted,
+        (),
+        ResultPushError.PushAlreadyRunning(tableId, destinationId))
+
+      pushMeta = PushMeta(path, format, limit, Status.running(now))
+
+      _ <- EitherT.right[RPE](Concurrent[F] delay {
+        pushStatus
+          .computeIfAbsent(destinationId, _ => new ConcurrentHashMap[T, PushMeta]())
+          .put(tableId, pushMeta)
+      })
 
     } yield ()
 
-    writing.run.map(Condition.disjunctionIso.reverseGet(_))
+    writing.value.map(Condition.eitherIso.reverseGet(_))
   }
 
-  def cancel(tableId: T): F[Condition[ExistentialError[T, D]]] =
-    (for {
-      _ <- ensureTableExists[ExistentialError[T, D]](tableId)
-      result <- EitherT.rightT(jobManager.cancel(tableId))
-      now <- EitherT.rightT(instantNow)
-      statusUpdate = Concurrent[F].delay(pushStatus.computeIfPresent(tableId, {
-        case (_, pm @ PushMeta(_, _, _, Status.Running(startedAt), _)) =>
-          pm.copy(status = Status.canceled(startedAt, now))
-        case (_, pm) =>
-          pm
-      }))
-      _ <- EitherT.rightT(statusUpdate)
-    } yield result).run.map(Condition.disjunctionIso.reverseGet(_))
+  def cancel(tableId: T, destinationId: D): F[Condition[ExistentialError[T, D]]] = {
+    val doCancel: F[Unit] = for {
+      result <- jobManager.cancel((destinationId, tableId))
 
-  def status(tableId: T): F[TableNotFound[T] \/ Option[PushMeta[D]]] =
-    (ensureTableExists[TableNotFound[T]](tableId) *>
-      EitherT.rightT(Concurrent[F].delay(Option(pushStatus.get(tableId))))).run
+      now <- instantNow
+
+      _ <- Concurrent[F] delay {
+        pushStatus.computeIfPresent(destinationId, (_, mm) => {
+          mm.computeIfPresent(tableId, {
+            case (_, pm @ PushMeta(_, _, _, Status.Running(startedAt))) =>
+              pm.copy(status = Status.canceled(startedAt, now))
+            case (_, pm) =>
+              pm
+          })
+
+          mm
+        })
+      }
+    } yield result
+
+    ensureBothExist[ExistentialError[T, D]](destinationId, tableId)
+      .semiflatMap(_ => doCancel)
+      .value
+      .map(Condition.eitherIso.reverseGet(_))
+  }
+
+  def destinationStatus(destinationId: D): F[Either[DestinationNotFound[D], Map[T, PushMeta]]] =
+    ensureDestinationExists[DestinationNotFound[D]](destinationId)
+      .semiflatMap(_ => Concurrent[F] delay {
+        Option(pushStatus.get(destinationId))
+          .fold(Map[T, PushMeta]())(_.asScala.toMap)
+      })
+      .value
 
   def cancelAll: F[Unit] =
-    jobManager.jobIds
-      .flatMap(Traverse[List].traverse(_)(jobManager.cancel(_))).void
+    jobManager.jobIds.flatMap(_.traverse_(jobManager.cancel(_)))
+
+
+  ////
 
   private def instantNow: F[Instant] =
     Timer[F].clock.realTime(MILLISECONDS)
       .map(Instant.ofEpochMilli(_))
 
-  private def findCsvSink(sinks: NonEmptyList[ResultSink[F]]): Option[ResultSink.Csv[F]] =
-    sinks.findMapM[Id.Id, ResultSink.Csv[F]] {
-      case csvSink @ ResultSink.Csv(_, _) => csvSink.some
-      case _ => none
-    }
+  private def findCsvSink(sinks: scalaz.NonEmptyList[ResultSink[F]]): Option[ResultSink.Csv[F]] = {
+    import scalaz.Id
+    import scalaz.syntax.foldable._
 
-  private def ensureTableExists[E >: TableNotFound[T] <: ResultPushError[T, D]](tableId: T)
+    sinks.findMapM[Id.Id, ResultSink.Csv[F]] {
+      case csvSink @ ResultSink.Csv(_, _) => Some(csvSink)
+      case _ => None
+    }
+  }
+
+  private def ensureBothExist[E >: ExistentialError[T, D] <: ResultPushError[T, D]](
+      destinationId: D,
+      tableId: T)
+      : EitherT[F, E, Unit] =
+    ensureDestinationExists[E](destinationId) *> ensureTableExists[E](tableId)
+
+  private def ensureDestinationExists[E >: DestinationNotFound[D] <: ResultPushError[T, D]](
+      destinationId: D)
+      : EitherT[F, E, Unit] =
+    OptionT(lookupDestination(destinationId))
+      .toRight[E](ResultPushError.DestinationNotFound(destinationId))
+      .void
+
+  private def ensureTableExists[E >: TableNotFound[T] <: ResultPushError[T, D]](
+      tableId: T)
       : EitherT[F, E, Unit] =
     OptionT(lookupTable(tableId))
       .toRight[E](ResultPushError.TableNotFound(tableId))
       .void
-
-  private def liftOptionF[F[_]: Functor, E, A](oa: F[Option[A]], err: E): EitherT[F, E, A] =
-    OptionT(oa).toRight[E](err)
 }
 
 object DefaultResultPush {
   def apply[F[_]: Concurrent: Timer, T, D, Q, R](
-    lookupTable: T => F[Option[TableRef[Q]]],
-    evaluator: QueryEvaluator[F, Q, Stream[F, R]],
-    lookupDestination: D => F[Option[Destination[F]]],
-    jobManager: JobManager[F, T, Nothing],
-    render: ResultRender[F, R]
-  ): F[DefaultResultPush[F, T, D, Q, R]] = {
+      lookupTable: T => F[Option[TableRef[Q]]],
+      evaluator: QueryEvaluator[F, Q, Stream[F, R]],
+      lookupDestination: D => F[Option[Destination[F]]],
+      jobManager: JobManager[F, (D, T), Nothing],
+      render: ResultRender[F, R])
+      : F[DefaultResultPush[F, T, D, Q, R]] =
     for {
-      pushStatus <- Concurrent[F].delay(new ConcurrentHashMap[T, PushMeta[D]]())
+      pushStatus <- Concurrent[F].delay(new ConcurrentHashMap[D, JMap[T, PushMeta]]())
+
       // we can't keep track of Completed and Failed jobs in this impl, so we consume them from JobManager
       // and update internal state accordingly
-      _ <- Concurrent[F].start((jobManager.events.evalMap {
-        case JobEvent.Completed(ti, start, duration) => {
+      _ <- Concurrent[F].start((jobManager.events evalMap {
+        case JobEvent.Completed((di, ti), start, duration) =>
           Concurrent[F].delay(
-            pushStatus.computeIfPresent(ti, {
-              case (_, pm) =>
-                pm.copy(status = Status.finished(
-                  epochToInstant(start.epoch),
-                  epochToInstant(start.epoch + duration)))
-            }))
-        }
+            pushStatus.computeIfPresent(di, (_, mm) => {
+              mm.computeIfPresent(ti, {
+                case (_, pm) =>
+                  pm.copy(status = Status.finished(
+                    epochToInstant(start.epoch),
+                    epochToInstant(start.epoch + duration)))
+              })
 
-        case JobEvent.Failed(ti, start, duration, ex) =>
+              mm
+            }))
+
+        case JobEvent.Failed((di, ti), start, duration, ex) =>
           Concurrent[F].delay(
-            pushStatus.computeIfPresent(ti, {
-              case (_, pm) =>
-                pm.copy(status = Status.failed(
-                  ex,
-                  epochToInstant(start.epoch),
-                  epochToInstant(start.epoch + duration)))
+            pushStatus.computeIfPresent(di, (_, mm) => {
+              mm.computeIfPresent(ti, {
+                case (_, pm) =>
+                  pm.copy(status = Status.failed(
+                    ex,
+                    epochToInstant(start.epoch),
+                    epochToInstant(start.epoch + duration)))
+              })
+
+              mm
             }))
       }).compile.drain)
     } yield new DefaultResultPush(lookupTable, evaluator, lookupDestination, jobManager, render, pushStatus)
-  }
 
   private def epochToInstant(e: FiniteDuration): Instant =
     Instant.ofEpochMilli(e.toMillis)
