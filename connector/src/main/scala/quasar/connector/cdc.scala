11bebc6674cee3fd6d4bce548fa3961ef2ca373f
Emrys Ingersoll <ingersoll@gmail.com>
2020-01-21 17:16:02 -0600

    moar wip

--- connector/src/main/scala/quasar/connector/cdc.scala
+++ connector/src/main/scala/quasar/connector/cdc.scala
@@ -21,31 +21,33 @@ package quasar.connector
 
 // Changes
 
-sealed trait Change[+F[_], +A] extends Product with Serializable
+// Types, existentials or not?
+//
+sealed trait Change[+F[_], +A] extends Product with Serializable {
+  def endOffset: ???
+}
 
-final case class Delete(correlationIds: List[???]) extends Change[Nothing, Nothing]
+final case class Delete(correlationIds: List[???], endOffset: ???) extends Change[Nothing, Nothing]
 // or maybe
-final case class Delete2(correlationIds: Stream[F, ???]) extends Change[F, Nothing]
+final case class Delete2(correlationIds: Stream[F, ???]), endOffset: ???) extends Change[F, Nothing]
 
 // AdditiveChange[F, A] ???
 sealed trait NonReductiveChange[+F[_], +A] extends Change[F, A]
 
 final case class Create[F[_], A](
   stages: ScalarStages,
-  // should this be a stream, or some sort of eager chunk?
-  data: Stream[F, A])
+  data: Stream[F, A],
+  endOffset: ???)
   extends NonReductiveChange[F, A]
 
 final case class Replace[F[_], A](
   stages: ScalarStages,
   correlationId: ???,
+  endOffset: ???,
   data: Stream[F, A])
   extends NonReductiveChange[F, A]
 
-final case class Mark(offset: ???, fixity: ???) extends Change[Nothing, Nothing]
-
 // Result
-
 sealed trait CDCResult[F[_], C <: Change] extends Product with Serializable
 
 final case class Parsed[F[_], A, C <: Change[F, A]](
@@ -67,7 +69,20 @@ final case class Stateful[F[_], C <: Change[F, Byte], P <: Plate[Unit], S](
 		extends CDCResult[F]
 
 
-trait SomethingDatasource[F[_]] extends Datasource[F, InterpretedRead[Path], CDCResult[F]]
+/*
+ * Datasource needs to provide the offset somehow
+ *
+ * Keep in mind at least once delivery of events.
+ */
+trait SomethingDatasource[F[_]] extends Datasource[F, (InterpretedRead[Path], Option[Offset]), CDCResult[F]] {
+  ???
+}
+
+// One of these per-consumption capability? Just start with all Changes
+trait SomethingElseDestination[F[_]] extends Destination[F[_]] {
+  // new column information representing correlation id/primary key
+  // a way to consume a change stream
+}
 
 // something like this?
 def evaluate[C <: Change[F, A]](changes: Stream[F, C]): Stream[F, Change[MimirRepr]] =
