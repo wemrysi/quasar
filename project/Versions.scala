a8db912fe0ea3d7efc267850e5e7edaf9a9dacc5
Scala Steward <me@scala-steward.org>
2020-01-29 07:50:17 +0100

    Update pathy-argonaut, pathy-core to 0.3.0

--- project/Versions.scala
+++ project/Versions.scala
@@ -10,7 +10,7 @@ object Versions {
   val jawnfs2Version      = "0.15.0"
   val matryoshkaVersion   = "0.18.3"
   val monocleVersion      = "1.6.0"
-  val pathyVersion        = "0.2.13"
+  val pathyVersion        = "0.3.0"
   val refinedVersion      = "0.9.10"
   val scodecBitsVersion   = "1.1.12"
   val scalacheckVersion   = "1.14.3"
