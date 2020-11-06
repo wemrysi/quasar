29214aaf0fb331658a51f071909d6b170e600686
Daniel Sobral <dcsobral@precog.com>
2020-11-06 12:54:20 -0700

    Move logFirstNChunks from sdbe, add test cases

--- impl/src/main/scala/quasar/impl/logging/LoggingUtils.scala
+++ impl/src/main/scala/quasar/impl/logging/LoggingUtils.scala
@@ -17,18 +17,23 @@
 package quasar.impl
 package logging
 
-import scala._, Predef._
+import java.nio.charset.StandardCharsets
 
+import scala._
+import Predef._
 import cats.effect.Sync
-
 import fs2.{Chunk, Stream}
 
 object LoggingUtils {
   def logFirstN[F[_]: Sync](s: Stream[F, Byte], max: Int, log: String => F[Unit])
       : Stream[F, Byte] = {
     def logChunk(bytes: Chunk[Byte]) =
-      log(new String(bytes.toArray, "utf-8"))
+      log(new String(bytes.toArray, StandardCharsets.UTF_8))
 
+    logFirstNChunks(s, max, logChunk)
+  }
+
+  def logFirstNChunks[F[_]: Sync](s: Stream[F, Byte], max: Int, logChunk: Chunk[Byte] => F[Unit]): Stream[F, Byte] = {
     s.chunks.zipWithIndex flatMap {
       case (bytes, i) if i < max =>
         Stream.eval_(logChunk(bytes)) ++ Stream.chunk(bytes)
